d9d7a234697f81babd5d75a5e5fa127aeb054d74
alexr007 <alexey.rykhalskiy@gmail.com>
2019-03-07 21:43:07 +0200

    implicit class

--- src/main/scala/x001/AddingMethods.scala
+++ src/main/scala/x001/AddingMethods.scala
@@ -2,8 +2,12 @@ package x001
 
 object AddingMethods extends App {
   implicit class StringXX(s: String) {
-    def increment = s.map(c => (c+1).toChar)
+    def increment = s.map(c => (c + 1).toChar)
+    def decrement = s.map(c => (c - 1).toChar)
+    def hidden = s.replaceAll(".", "*")
   }
 
   println("HAL".increment)
+  println("HAL".decrement)
+  println("Hello".hidden)
 }
