c2bc37f03aa48c4257328c574af7ed234ad682ec
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2019-03-09 17:49:24 +0200

    ch3 in process

---
+++ src/main/scala/x003/P34Yield.scala
@@ -0,0 +1,16 @@
+package x003
+
+import scala.util.Random
+
+object P34Yield extends App {
+  val r = Random
+  var ra = for (i <- 1 to 50) yield r.nextInt(20)
+  val ra2 = for {
+    i <- ra.indices
+    if ra(i) < 10
+  } yield ra(i)
+  println(ra.mkString(","))
+  println(ra2.mkString(","))
+  println(ra2.distinct.mkString(","))
+  println(ra2.distinct.sorted.mkString(","))
+}
