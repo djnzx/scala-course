9a51aa1353097495e015c7b2262e7bf791608762
alexr007 <alexey.rykhalskiy@gmail.com>
2019-03-10 15:40:22 +0200

    chapter 4 done

---
+++ src/main/scala/x004/NiceTransformations.scala
@@ -0,0 +1,18 @@
+package x004
+
+object NiceTransformations extends App {
+  "This is the test string"
+    .zipWithIndex
+    // java filtering way
+    .filter(t => Character.isAlphabetic(t._1))
+    // scala filtering way
+    //.filter{ case (c, _) => c.isLetter }
+    .groupBy (t => t._1)
+    .mapValues(el => el.map(_._2))
+    .mapValues(_.mkString("<",".",">"))
+    .toList
+    .sortBy(t => t._1)
+    .map(t => s"${t._1}:${t._2}")
+    .mkString(", ")
+    .foreach(print)
+}
