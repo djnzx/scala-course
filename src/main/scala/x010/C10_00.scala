6dfe0e171de791e97fe761621ad547cf67d533c9
alexr007 <alexey.rykhalskiy@gmail.com>
2019-04-08 06:53:49 +0300

    ch 10.2 done

---
+++ src/main/scala/x010/C10_00.scala
@@ -0,0 +1,19 @@
+package x010
+
+object C10_00 extends App {
+  val list = List.range(1,10)
+  val evens = list.filter(_ % 2 == 0)
+  val double = evens.map(_ * 2)
+
+  println(list)
+  println(evens)
+  println(double)
+
+  val same = for {
+    e <- list
+    if e % 2 ==0
+  } yield e
+
+  println(same)
+
+}
