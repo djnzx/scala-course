72cb9ff83bb16c4359f9fd3956b1e8b253ac6bdc
alexr007 <alexey.rykhalskiy@gmail.com>
2019-07-22 17:32:43 +0300

    implicits in process...

--- src/main/scala/_implicits/x8typeclass/WithTypeClassesImplParamsConvers.scala
+++ src/main/scala/_implicits/x8typeclass/WithTypeClassesImplParamsConvers.scala
@@ -8,8 +8,8 @@ object WithTypeClassesImplParamsConvers extends App {
   val dog = Dog("Jack")
 
   // implicit declaration which will be attached to any type A
-  implicit class ChatUtil[A](x: A) {
-    def chat(implicit makesChatty: CanChat[A]): String = makesChatty.chat_(x)
+  implicit class ChatMixin[A](x: A) {
+    def chat(implicit canChat: CanChat[A]): String = canChat.chat_(x)
   }
   // implicit implementations, not tied
   implicit object PersonCanChat extends CanChat[Person] {
@@ -21,16 +21,35 @@ object WithTypeClassesImplParamsConvers extends App {
   implicit object StringCanChat extends CanChat[String] {
     def chat_(a: String): String = s"I'm a string with a val:$a"
   }
-  implicit object IntCanChat extends CanChat[Int] {
-    def chat_(a: Int): String = s"I'm an int with a val:$a"
+
+  /** declaring implementation
+    * way 1 - Object (Singleton, class with one static method)
+    */
+//  implicit object IntCanChat extends CanChat[Int] {
+//    def chat_(a: Int): String = s"I'm an int with a val:$a"
+//  }
+  /** declaring implementation
+    * way 2 - making instance of Interface with according type
+    */
+  implicit val intCanChat = new CanChat[Int] {
+    override def chat_(a: Int): String = s"I'm an int with a val:$a")
   }
 
-  // here we call ChatUtil.chat
+  // here we call .chat() from ChatUtil.chat
   val s1 = person.chat
   val s2 = dog.chat
   val s3 = "STRING".chat
-  val s4 = 1.chat
-  println(s"$s1\n$s2\n$s3\n$s4\n")
-
-
+  val s41 = 1.chat
+  /** declaring implementation
+    * way 3 - inlined implementation of Interface with according type
+    */
+  val s42 = 1.chat( new CanChat[Int]() {
+    override def chat_(a: Int): String = s"I'm an int with a val:$a"
+  })
+  /** declaring implementation
+    * way 4 - inlined implementation of Interface with according type
+    * simplified syntax if only one method in the interface
+    */
+  val s43 = 1.chat((a: Int) => s"I'm an int with a val:$a")
+  println(s"$s1\n$s2\n$s3\n$s41\n")
 }
