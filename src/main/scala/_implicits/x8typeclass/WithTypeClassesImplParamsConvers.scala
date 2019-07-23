2f82e8e89e3ce3e3274807a099b7de1a77ad8c68
alexr007 <alexey.rykhalskiy@gmail.com>
2019-07-23 18:24:19 +0300

    implicits in process...

--- src/main/scala/_implicits/x8typeclass/WithTypeClassesImplParamsConvers.scala
+++ src/main/scala/_implicits/x8typeclass/WithTypeClassesImplParamsConvers.scala
@@ -25,15 +25,15 @@ object WithTypeClassesImplParamsConvers extends App {
   /** declaring implementation
     * way 1 - Object (Singleton, class with one static method)
     */
-//  implicit object IntCanChat extends CanChat[Int] {
-//    def chat_(a: Int): String = s"I'm an int with a val:$a"
-//  }
+  implicit object IntCanChat extends CanChat[Int] {
+    def chat_(a: Int): String = s"I'm an int (local implementation has higher priority than imported) with a val:$a"
+  }
   /** declaring implementation
     * way 2 - making instance of Interface with according type
     */
-  implicit val intCanChat = new CanChat[Int] {
-    override def chat_(a: Int): String = s"I'm an int with a val:$a")
-  }
+//  implicit val intCanChat = new CanChat[Int] {
+//    override def chat_(a: Int): String = s"I'm an int (implicit val) with a val:$a"
+//  }
 
   // here we call .chat() from ChatUtil.chat
   val s1 = person.chat
@@ -44,12 +44,12 @@ object WithTypeClassesImplParamsConvers extends App {
     * way 3 - inlined implementation of Interface with according type
     */
   val s42 = 1.chat( new CanChat[Int]() {
-    override def chat_(a: Int): String = s"I'm an int with a val:$a"
+    override def chat_(a: Int): String = s"I'm an int (inline implementation #1) with a val:$a"
   })
   /** declaring implementation
     * way 4 - inlined implementation of Interface with according type
     * simplified syntax if only one method in the interface
     */
-  val s43 = 1.chat((a: Int) => s"I'm an int with a val:$a")
-  println(s"$s1\n$s2\n$s3\n$s41\n")
+  val s43 = 1.chat((a: Int) => s"I'm an int (inline implementation #2) with a val:$a")
+  println(s"$s1\n$s2\n$s3\n$s41\n$s42\n$s43\n")
 }
---
+++ src/main/scala/_implicits/x8typeclass/_implicits.scala
@@ -0,0 +1,8 @@
+import _implicits.x8typeclass.CanChat
+
+package object _implicits {
+  implicit val intCanChat = new CanChat[Int] {
+    override def chat_(a: Int): String = s"I'm an int (implicit val imported from other class) with a val:$a"
+  }
+
+}
