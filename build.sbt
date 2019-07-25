0c91428c592c3e34d5404534701837fbcb033111
alexr007 <alexey.rykhalskiy@gmail.com>
2019-07-25 23:41:28 +0300

    slick

--- build.sbt
+++ build.sbt
@@ -3,12 +3,18 @@ name := "learn-scala-cook-book-aa"
 version := "0.2"
 scalaVersion := "2.12.8"
 
-resolvers ++= Seq(
-  "Typesafe" at "http://repo.typesafe.com/typesafe/releases/",
+//resolvers ++= Seq(
+//  "Typesafe" at "http://repo.typesafe.com/typesafe/releases/",
 //  "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
-)
+//)
 
 libraryDependencies ++= Seq(
   "org.scala-lang.modules" %% "scala-xml" % "1.0.6",
-  "com.typesafe.akka" %% "akka-actor" % "2.5.23"
+  "com.typesafe.akka" %% "akka-actor" % "2.5.23",
+  "com.typesafe.slick" %% "slick" % "3.3.2",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.3.2",
+  "org.postgresql" % "postgresql" % "42.2.5",
+//  "org.postgresql" % "postgresql" % "9.4-1206-jdbc42", //
+//  "com.h2database" % "h2" % "1.4.197",
+  "ch.qos.logback" % "logback-classic" % "1.2.3"
 )
--- src/main/scala/x013actors/ActorApp01.scala
+++ src/main/scala/x013actors/ActorApp01.scala
@@ -4,20 +4,40 @@ import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 class HelloActor extends Actor {
   override def receive: Receive = {
-    case "hello" => println("I've got it")
-    case _       => println("?")
+    case "hello"   => println("I've got it")
+    case "forward" => println(s"got 'forward' from $sender")
+    case _         => println("?")
+  }
+}
+
+class NamedActor(name: String, hell: ActorRef) extends Actor {
+  override def receive: Receive = {
+    case "hello" => println(s"$name: 'Hello' got it")
+    case "FWD"   =>
+      println(s"$name: got 'FWD' from $sender")
+      hell ! "forward"
+    case _       => println(s"$name: ?")
   }
 }
 
 object ActorApp extends App {
-  // parent system
-  val system: ActorSystem = ActorSystem("hello_system")
+  // parent system creating
+  val system: ActorSystem = ActorSystem("hello_system") // akka://hello_system/deadLetters
 
-  // our actor
+  // actor creating
   val hello_actor: ActorRef = system.actorOf(Props[HelloActor], name = "hello_actor")
+  val fred_actor: ActorRef = system.actorOf(Props(new NamedActor("FRED", hello_actor)), name = "fred_actor")
 
+  println(system)      // akka://hello_system
+  println(hello_actor) // Actor[akka://hello_system/user/hello_actor#-978228995]
+  println(fred_actor)  // Actor[akka://hello_system/user/fred_actor#1781372986]
+
+  // these operations are async!
+  hello_actor ! "forward" // got 'forward' from Actor[akka://hello_system/deadLetters]
   hello_actor ! "hello"
   hello_actor ! "Hi, there!"
+  fred_actor ! "HI"
+  fred_actor ! "FWD"
 
   system.terminate()
 }
