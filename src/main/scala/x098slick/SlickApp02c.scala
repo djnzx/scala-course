59e9095a581bbce8767949c2613379bbd33f6318
Alexey <alexey.rykhalskiy@gmail.com>
2019-08-02 16:46:32 +0300

    - limit + offset

--- src/main/scala/x098slick/SlickApp02c.scala
+++ src/main/scala/x098slick/SlickApp02c.scala
@@ -11,25 +11,31 @@ object SlickApp02c extends App {
 
   val action: Query[MessageTable, MessageTable#TableElementType, Seq] = messages.filter({mt: MessageTable => mt.id > 2L})
   println( action.result.statements.mkString)
+  // sortBy
   val action1a = action.sortBy(_.sender)
   println(action1a.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender"
   val action1b = action.sortBy(_.sender.desc)
   println(action1b.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender" desc
   val action1c = action.sortBy(el => (el.sender.desc, el.content.asc))
   println(action1c.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender" desc, "content"
+  val action1d = action.sortBy(_.sender.nullsFirst)
+  println(action1d.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender" nulls first
+  val action1e = action.sortBy(_.sender.nullsLast)
+  println(action1e.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender" nulls last
+  val action1f = action.sortBy(_.sender.nullsDefault)
+  println(action1f.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender"
 
-  val action2: Query[Rep[String], String, Seq] = action.map(_.content)
-  val action4 = action.map(x => (x.id, x.sender ++ ">" ++ x.content toLowerCase))
+  // take
+  val action2a = action.take(2)
+  println(action2a.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 limit 2
 
-  val dbio: DBIO[Seq[String]] = action2.result
-  val future: Future[Seq[String]] = db.run(dbio)
-  val resolved: Seq[String] = Await.result(future, 2 seconds)
-  resolved foreach println
+  // drop
+  val action3a = action.drop(2)
+  println(action3a.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 offset 2
 
-  val dbio_ = action4.result
-  val future_ = db.run(dbio_)
-  Await.result(future_, 10 second) foreach println
+  // drop + take
+  val action3b = action.drop(2).take(2)
+  println(action3b.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 limit 2 offset 2
 
-  val e = Await.result(db.run(action4.exists.result), 10 second)
-  println(e)
+//  val e = Await.result(db.run(action4.exists.result), 10 second)
 }
