0de8d4c998576466e3051e988718372688d5ace4
Alexey <alexey.rykhalskiy@gmail.com>
2019-08-02 16:35:22 +0300

    slick 2.04 - order

--- src/main/scala/x098slick/SlickApp02c.scala
+++ src/main/scala/x098slick/SlickApp02c.scala
@@ -2,27 +2,24 @@ package x098slick
 
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-object SlickApp02b extends App {
-
-//  case class TextOnly(id: Long, content: String)
+object SlickApp02c extends App {
 
   println( messages.result.statements.mkString)
 
   val action: Query[MessageTable, MessageTable#TableElementType, Seq] = messages.filter({mt: MessageTable => mt.id > 2L})
   println( action.result.statements.mkString)
+  val action1a = action.sortBy(_.sender)
+  println(action1a.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender"
+  val action1b = action.sortBy(_.sender.desc)
+  println(action1b.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender" desc
+  val action1c = action.sortBy(el => (el.sender.desc, el.content.asc))
+  println(action1c.result.statements.mkString) // select "sender", "content", "id" from "message" where "id" > 2 order by "sender" desc, "content"
 
   val action2: Query[Rep[String], String, Seq] = action.map(_.content)
-  val action3: Query[(Rep[String], Rep[String]), (String, String), Seq] = action.map(x => (x.content, x.sender))
-//  val action4: Query[(Rep[Long], Rep[String], Rep[String]), (Long, String, String), Seq] = action.map(x => (x.id * 1000L, x.content, x.sender))
-//  val action4: Query[TextOnly, (Long, String), Seq] = action.map(x => (x.id, x.sender).mapTo[TextOnly])
   val action4 = action.map(x => (x.id, x.sender ++ ">" ++ x.content toLowerCase))
-  println( action2.result.statements.mkString)
-  println( action3.result.statements.mkString)
-  println( action4.result.statements.mkString)
-  println( action4.exists.result.statements.mkString)
 
   val dbio: DBIO[Seq[String]] = action2.result
   val future: Future[Seq[String]] = db.run(dbio)
@@ -35,6 +32,4 @@ object SlickApp02b extends App {
 
   val e = Await.result(db.run(action4.exists.result), 10 second)
   println(e)
-//  val ex = Await.result(db.run(messagesO.result), 10 second)
-//  println(ex)
 }
--- src/main/scala/x098slick/package.scala
+++ src/main/scala/x098slick/package.scala
@@ -6,6 +6,7 @@ import scala.concurrent.duration._
 
 package object x098slick {
   final case class Message2(sender: String, content: String, id: Long = 0)
+  final case class Message20(sender: String, content: Option[String], id: Long = 0)
 
   final class MessageTable(tag: Tag) extends Table[Message2](tag, _tableName = "message") {
     def id      = column[Long]("id", O.PrimaryKey, O.AutoInc) // Unique, default value
@@ -14,6 +15,13 @@ package object x098slick {
     // mapping function: tuple => ...
     def * = (sender, content, id).mapTo[Message2]
   }
+  final class MessageTable0(tag: Tag) extends Table[Message20](tag, _tableName = "message") {
+    def id      = column[Long]("id", O.PrimaryKey, O.AutoInc) // Unique, default value
+    def sender  = column[String]("sender")
+    def content = column[Option[String]]("content")
+    // mapping function: tuple => ...
+    def * = (sender, content, id).mapTo[Message20]
+  }
   val db: Database = Database.forConfig("chapter01my")
 
   // Helper method for running a query in this example file:
@@ -21,5 +29,6 @@ package object x098slick {
 
   // Base query for querying the messages table: definition only
   lazy val messages: TableQuery[MessageTable] = TableQuery[MessageTable]
+  lazy val messagesO: TableQuery[MessageTable0] = TableQuery[MessageTable0]
 
 }
