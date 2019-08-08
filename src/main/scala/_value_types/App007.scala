4ce96a437b4b684d8e3576d68866ee60d7a9cc36
Alexey <alexey.rykhalskiy@gmail.com>
2019-08-08 21:46:39 +0300

    -- value types

--- src/main/scala/_value_types/App007.scala
+++ src/main/scala/_value_types/App007.scala
@@ -7,7 +7,9 @@ object App007 extends App {
   val i11 = PersonInt(1)
   val i12 = PersonInt(1)
   val i2 = BookInt(1)
-  if (i11 == i12) println("Ok")
+  if (i11 == i2) println("Different types")
+  if (i11 == i12) println("Similar types")
+  if (i11.x == 1) println("Unboxing OK")
 
 
 }
