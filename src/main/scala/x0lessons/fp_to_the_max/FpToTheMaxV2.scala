008d341be521b0458842d8928cd2e9dea07321b1
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-27 17:21:27 +0400

    -- tests, but w/o implementation

--- src/main/scala/x0lessons/fp_to_the_max/FpToTheMaxV2.scala
+++ src/main/scala/x0lessons/fp_to_the_max/FpToTheMaxV2.scala
@@ -4,6 +4,35 @@ import scala.language.higherKinds
 
 object FpToTheMaxV2 extends App {
 
+  case class TestData(input: List[String], output: List[String], nums: List[Int])
+
+  case class TestIO[A](run: TestData => (TestData, A)) { self =>
+    def map[B](fab: A => B): TestIO[B] =
+      TestIO(t => self.run(t) match { case (t, a) => (t, fab(a)) })
+    def flatMap[B](afb: A => TestIO[B]): TestIO[B] =
+      TestIO(t => self.run(t) match { case (t, a) => afb(a).run(t) })
+  }
+  object TestIO {
+    def point[A](a: => A): TestIO[A] = TestIO(t => (t, a))
+
+    implicit val ProgramTestIO: Program[TestIO] = new Program[TestIO] {
+      override def finish[A](a: => A): TestIO[A] = TestIO.point(a)
+      override def chain[A, B](fa: TestIO[A], afb: A => TestIO[B]): TestIO[B] = fa.flatMap(afb)
+      override def map[A, B](fa: TestIO[A], ab: A => B): TestIO[B] = fa.map(ab)
+    }
+
+    // mock
+    implicit val ConsoleTestIO: Console[TestIO] = new Console[TestIO] {
+      override def putStrLn(line: String): TestIO[Unit] = ???
+      override def getStrLn(): TestIO[String] = ???
+    }
+
+    // mock
+    implicit val RandomTestIO: Random[TestIO] = new Random[TestIO] {
+      override def nextInt(upper:  Int): TestIO[Int] = ???
+    }
+  }
+
   def checkContinue[F[_]: Program: Console](name: String): F[Boolean] =
     for {
       _     <- putStrLn(s"Do you want to continue, $name?")
@@ -41,5 +70,5 @@ object FpToTheMaxV2 extends App {
 
   def mainIO = main[IO].core()
 
-  mainIO
+  def mainTestIO = main[TestIO]
 }
