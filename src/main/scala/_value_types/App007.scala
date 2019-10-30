68dcd21d3c4537fa6461fac4423de81a43f7c430
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-30 14:24:08 +0400

    -- value types polished

--- src/main/scala/_value_types/App007.scala
+++ src/main/scala/_value_types/App007.scala
@@ -2,16 +2,26 @@ package _value_types
 
 object App007 extends App {
 
-  case class PersonInt(x: Int) extends AnyVal
-  case class BookInt(x: Int) extends AnyVal
+  def doCheck(cond: Boolean): String = if (cond) "OK" else "ERR"
 
-  val person1a = PersonInt(1)
-  val person1b = PersonInt(1)
-  val book1 = BookInt(1)
+  case class PersonId(private val src: Int) extends AnyVal {
+    def id = this.src // + 1
+  }
+  case class BookId(src: Int) extends AnyVal
 
-  if (person1a == person1b) println("Comparing similar types.... OK")
+  val just1   : Int       = 1
+  val person1a: PersonId = PersonId(1)
+  val person1b: PersonId = PersonId(1)
+  val book1   : BookId   = BookId(1)
 
-  if (person1a == book1) println("Different types") // false
-  if (person1a.x == 1) println("Comparing Typed (with unboxing) an Untyped... OK")
-  if (book1.x == 1) println("Comparing Typed (with unboxing) an Untyped... OK")
+  val testData = List(
+    "Similar Value types......................... %s" -> (person1a == person1b),
+    "Different value types....................... %s" -> (person1a == book1),   // false
+    "Value type vs Int........................... %s" -> (person1a == just1),   // false
+    "Value type vs Literal....................... %s" -> (person1a == 1),       // false
+    "Value type unboxed vs Literal............... %s" -> (book1.src == 1),
+    "Value type extracted by getter vs Literal... %s" -> (person1a.id == 1),
+    "Value type extracted by getter vs Int....... %s" -> (person1a.id == just1),
+  )
+  testData. foreach(el => printf(el._1+"\n", doCheck(el._2)))
 }
