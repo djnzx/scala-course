0c64867a4c376801a4d913e8583fcbd6c1e5f44c
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-31 08:16:40 +0400

    -- NamedMixins.scala

---
+++ src/main/scala/x99/NamedMixins.scala
@@ -0,0 +1,26 @@
+package x99
+
+trait Vehicle
+
+trait Horn {
+  def toot(): Unit
+}
+
+trait LoudHorn extends Horn {
+  override def toot(): Unit = println("TOOT!")
+}
+
+trait SoftHorn extends Horn {
+  override def toot(): Unit = println("toot")
+}
+
+class Car extends Vehicle { this: Horn =>
+}
+
+class CarSoft extends Car with SoftHorn
+class CarLoud extends Car with LoudHorn
+
+object NamedMixins extends App {
+  new CarSoft().toot()
+  new CarLoud().toot()
+}
