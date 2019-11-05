45987a90001c9b5c036ff6e223f926270953e249
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-05 07:19:11 +0400

    -- recursion with accumulator

--- src/main/scala/x00topics/recursion/Recursion1.scala
+++ src/main/scala/x00topics/recursion/Recursion1.scala
@@ -5,6 +5,13 @@ import scala.annotation.tailrec
 object Recursion1 extends App {
   val list = 1 to 1_000_000 toList
 
+  // this function can't be optimized for tail recursion
+  private def lenr[A](list: List[A]): Int = list match {
+    case Nil => 0
+    case _ :: tail => 1 + lenr(tail)
+  }
+
+  // this function can be optimized for tail recursion
   @tailrec
   private def lenar[A](list: List[A], ac: Int): Int = list match {
     case Nil => ac
