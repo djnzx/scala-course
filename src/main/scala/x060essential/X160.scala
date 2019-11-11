1a920a3c9bdae02d23fe6ec37088ada161366e54
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-11 23:20:29 +0400

    -- scala essential. p192 done

--- src/main/scala/x060essential/X160.scala
+++ src/main/scala/x060essential/X160.scala
@@ -1,5 +1,8 @@
 package x060essential
 
+import scala.collection.immutable.Queue
+import scala.collection.mutable
+
 object X160 extends App {
   /**
     * Sequence: defined and stable order
@@ -9,7 +12,7 @@ object X160 extends App {
     */
   class ABC {
     def apply(num: Int): ABC = {
-      println(num)
+//      println(num)
       new ABC()
     }
   }
@@ -34,13 +37,43 @@ object X160 extends App {
     * Seq by default is a List
     * List by default is a LinkedList
     * by default all is immutable
+    *
+    * :: and ::: List only operations
     */
-  val list1 = List(1,2,3)
-  val list2: Seq[Int] = 1 :: 2 :: 3 :: Nil
-  val list3: Seq[Any] = 1 :: "abc" :: true :: Nil
+  val list1: List[Int] = List(1,2,3)
+  val list2: List[Int] = 1 :: 2 :: 3 :: Nil
+  val list3: List[Any] = 1 :: "abc" :: true :: Nil
+  val list4: List[Int] = List(1,2,3) ::: List(4,5,6)
 
+  val v1: Vector[Int] = Vector(1,2,3,4,5)
+  val q1: Queue[Int] = Queue(1, 2, 3, 5, 6, 7)
+  val q2 = q1.prepended(44)               // add ONE  item  to the front of the queue
+  val q3 = q1.prependedAll(Seq(44,55,66)) // add MANY items to the front of the queue
 
+  val q51 = q1.appended(77)               // add ONE  item  to the end of the queue
+  val q52 = q1.appendedAll(Seq(77,88))    // add MANY items to the end of the queue
+  val q53 = q1.enqueue(99)                // -------- "" -------
+  val q54 = q1.enqueueAll(List(98,99))    // -------- "" -------
 
+  val (head, tail) = q1.dequeue           // take one from the queue (1,Queue(2, 3, 5, 6, 7)) or 'NoSuchElementException'
+  q1.dequeueOption                        // the same ^ but to option
+  val q60 = q1.drop(1)                    // DROP numbers of elements from the FRONT of the queue
+  val q61 = q1.dropRight(1)               // DROP numbers of elements from the END of the queue
+  val q62 = q1.head                       // take the head or Exception
+  val q63 = q1.headOption                 // take the head to Option w/o Exception
+  val q64 = q1.last                       // take the last element or Exception
+  val q65 = q1.lastOption                 // take the last element to Option w/o Exception
+  val q66 = q1.tail                       // tail of the queue
+  List(1,2,3).head                        // take
+  List(1,2,3).take(1)                     // take and remove
 
+  val l1: List[Int] = List(1,2)
+  // val l2: List[Int] = l1 :+ "abc" // error in compile time
+  val l3: List[Any] = l1 :+ "abc"
 
+  val hh = "abc".toSeq.permutations.toList
+  val pm1 = Seq("a", "wet", "dog").map(_.permutations.toList)
+  val pm2 = Seq("a", "wet", "dog").flatMap(_.permutations.toList)
+  println(pm1)
+  println(pm2)
 }
