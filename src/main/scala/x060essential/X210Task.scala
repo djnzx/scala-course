86f1b21fc8c94dcb349f6bcc794198aafe968c4e
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-18 11:14:08 +0400

    -- scala essential p.210 -- scala implementation #2

--- src/main/scala/x060essential/X210Task.scala
+++ src/main/scala/x060essential/X210Task.scala
@@ -15,12 +15,20 @@ object X210Task extends App {
     "meowed at" -> List("Noel", "the door", "the food cupboard"),
     "barked at" -> List("the postman", "the car", "the cat"),
   )
-  val total: immutable.Iterable[String] = for {
+  val total1: immutable.Iterable[String] = for {
     (s1, v1L) <- assoc_subj_verb // decompose to key, value
     v1 <- v1L                    // decompose List to Items
     (v2, o2L) <- assoc_verb_obj  // decompose to key, value
     o2 <- o2L                    // decompose List to Items
     if v1 == v2                  // make a filtering
-  } yield s"$s1 $v1 $o2"         // produce a result
-  total.foreach(println)
+  } yield s"$s1 $v1 $o2"
+
+  val total2: immutable.Iterable[String] = for {
+    (s, v_list) <- assoc_subj_verb   // decompose to key, value
+    v           <- v_list            // decompose List to Items
+    o           <- assoc_verb_obj(v) // get corresponding objects to verbs
+  } yield s"$s $v $o"
+
+  total1.foreach(println)
+  total2.foreach(println)
 }
