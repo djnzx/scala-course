3438a6e453ab940a60f284b58b6aad5befb72f64
alexr007 <alexey.rykhalskiy@gmail.com>
2020-01-28 11:52:51 +0200

    -- method vs function

--- src/main/scala/aa_fp/Fps002Array.scala
+++ src/main/scala/aa_fp/Fps002Array.scala
@@ -1,6 +1,6 @@
 package aa_fp
 
-object Fps002_Array extends App {
+object Fps002Array extends App {
   val v1 = Vector(1,2,3,4,5)
   val v2 = v1.updated(3,40)
   println(v1)
--- src/main/scala/aa_fp/Fps003MethodVsFunc.scala
+++ src/main/scala/aa_fp/Fps003MethodVsFunc.scala
@@ -1,12 +1,15 @@
 package aa_fp
 
-object Fps003 extends App {
+object Fps003MethodVsFunc extends App {
 
   val double1 = (i: Int) => i * 2
   def double2(i: Int): Int = i * 2
   val double3 = double2 _
 
   val l = List(1,2,3)
+
+  val set1 = Set(double1, double2 _)
+  val set2: Set[Int => Int] = Set(double1, double2)
 
   val l11 = l.map(double1)
   val l12 = l.map(double2)
