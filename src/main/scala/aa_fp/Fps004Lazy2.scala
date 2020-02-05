81f508471a9f30041dd4a09abfa3032161342d3c
Alexey <alexey.rykhalskiy@gmail.com>
2020-02-05 17:57:32 +0400

    -- laziness more clear example

--- src/main/scala/aa_fp/Fps004Lazy2.scala
+++ src/main/scala/aa_fp/Fps004Lazy2.scala
@@ -4,22 +4,39 @@ object Fps004Lazy2 extends App {
 
   var counter = 0;
 
-  val codeblock: () => Long = () => {
+  val block_of_code: () => Long = () => {
     println("evaluating")
     counter+=1
     counter
   }
 
-  def ex1(code: => Long): Unit = {
-  }
-  def ex2(code: => Long): Unit = {
-    println(code)
+  def ex1a(code: => Long): Unit = {
+    println("ex1a:entrance")
+    println(code)                  // code will be evaluated (called) HERE
+    println("ex1a:quit")
   }
 
-  ex1(codeblock())
-  ex1(codeblock())
-  ex2(codeblock())
-  ex2(codeblock())
-  println(counter)
+  def ex1b(code: => Long): Unit = { // code won't be evaluated (called)
+    println("ex1b:entrance")
+    println("ex1b:quit")
+  }
+
+  def ex2a(code: Long): Unit = {
+    println("ex2a:entrance")
+    println(code)
+    println("ex2a:quit")
+  }
+
+  def ex2b(code: Long): Unit = {
+    println("ex2b:entrance")
+    println("ex2b:quit")
+  }
+
+  ex1a(block_of_code()) // block_of_code MAY BE evaluated (called) INSIDE function, because of: `code: => Long`
+  ex1b(block_of_code()) // block_of_code MAY BE evaluated (called) INSIDE function, because of: `code: => Long`
+  ex2a(block_of_code()) // block_of_code WILL BE called (evaluated) HERE, because of: `code: Long`
+  ex2b(block_of_code()) // block_of_code WILL BE called (evaluated) HERE, because of: `code: Long`
+
+  println(counter) // 3 will be printed
 
 }
