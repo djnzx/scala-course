8c1e2ce54bcabb3f7806498172b38ad4f929aaa6
Alexey <alexey.rykhalskiy@gmail.com>
2020-02-08 23:26:01 +0400

    -- 8.02.2020 -- map / flatMap deep explanation

--- src/main/scala/aa_fp/Fps068.scala
+++ src/main/scala/aa_fp/Fps068.scala
@@ -2,15 +2,48 @@ package aa_fp
 
 object Fps068 extends App {
 
-  case class Debuggable(value: Int, message: String) {
-    def map(f: Int => Int): Debuggable = Debuggable(f(value), message)
-    def flatMap(f: Int => Debuggable): Debuggable =
-      f(value) match { case Debuggable(i,s) => Debuggable(i, s"$message $s") }
+  case class Debuggable(value: Int, msg: String) {
+    def map(f: Int => Int): Debuggable = {
+      println("\n>>>> entered map  >>>>")
+      println(s"map: value: ${value}")
+      println(s"map: msg: (${msg})")
+      val nextValue = f(value)   //Int
+      // there is no `nextValue.msg`
+      println(s"map: nextValue: ${nextValue}")
+      println("<<<< leaving map  <<<<\n")
+      Debuggable(nextValue, msg)
+    }
+
+    def flatMap(f: Int => Debuggable): Debuggable = {
+      println("\n>>>> entered fmap >>>>")
+      println(s"fmap: value: ${value}")
+      println(s"fmap: msg: (${msg})")
+      val nextValue = f(value)
+      println(s"fmap: msg: (${msg})")
+      println(s"fmap: next val: ${nextValue.value}")
+      println(s"fmap: next msg: \n(${nextValue.msg})")
+      println("<<<< leaving fmap <<<<\n")
+      Debuggable(nextValue.value, msg + "\n" + nextValue.msg)
+    }
   }
 
-  def f(x: Int): Debuggable = Debuggable(x + 2, s"f($x)=${x+2}")
-  def g(x: Int): Debuggable = Debuggable(x * 2, s"g($x)=${x*2}")
-  def h(x: Int): Debuggable = Debuggable(x * x, s"h($x)=${x*x}")
+  def f(a: Int): Debuggable = {
+    println(s"\n[f: a = $a]")
+    val result = a * 2
+    Debuggable(result, s"f: input: $a, result: $result")
+  }
+
+  def g(a: Int): Debuggable = {
+    println(s"\n[g: a = $a]")
+    val result = a * 3
+    Debuggable(result, s"g: input: $a, result: $result")
+  }
+
+  def h(a: Int): Debuggable = {
+    println(s"\n[h: a = $a]")
+    val result = a * 4
+    Debuggable(result, s"h: input: $a, result: $result")
+  }
 
   val r: Debuggable = for {
     fr <- f(100) // flatMap
@@ -19,6 +52,6 @@ object Fps068 extends App {
   } yield hr
 
   println(s"r.value = ${r.value}")
-  println(s"r.message = ${r.message}")
+  println(s"r.msg = ${r.msg}")
 
 }
