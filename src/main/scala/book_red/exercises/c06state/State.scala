bdfcee43f80334777e755d146a3b22a1b47ffa93
Alexey <alexey.rykhalskiy@gmail.com>
2020-03-01 12:13:24 +0400

    -- red book exercises: CH 6 in process......

--- src/main/scala/book_red/exercises/c06state/State.scala
+++ src/main/scala/book_red/exercises/c06state/State.scala
@@ -165,6 +165,16 @@ object RNGApp extends App {
     (list reverse, st_last)
   }
 
+  // scala foldLeft based solution
+  def sequenceScala[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight( unit(List[A]()) )(  (acc, elem) => map2(acc, elem)((a: A, b: List[A]) => a :: b)  )
+
+  def sequenceScala2[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight( unit(List[A]()) )(  (acc, ra) => map2(acc, ra)((a, b) => a :: b)  )
+
+  def sequenceScala3[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight( unit(List[A]()) )(  (acc, ra) => map2(acc, ra)( _ :: _ ) )
+
   println("==========")
   val tr1: Rand[Int] = s => s.nextInt
   val tr2: Rand[Int] = s => (2, s)
@@ -172,9 +182,11 @@ object RNGApp extends App {
   val ll1 = sequenceTR(List(tr1, tr2, tr3))(seed)._1
   val ll2 = sequenceR(List(tr1, tr2, tr3))(seed)._1
   val ll3 = sequence(List(tr1, tr2, tr3))(seed)._1
+  val ll4 = sequenceScala(List(tr1, tr2, tr3))(seed)._1
   println(ll1)
   println(ll2)
   println(ll3)
+  println(ll4)
   println("==========")
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = (s: RNG) => {
@@ -251,6 +263,62 @@ object RollDieStateApp extends App {
   // running representation and extracting value
   val (v, r) =  dice2.run(seed)
   println(v)
+
+  println("-- roll N times")
+
+  // roll classic recursive implementation
+  def roll1(num: Int): State[RNG, List[Int]] = num match {
+    case 0 => State(s => (Nil, s)) // recursion exit
+    case _ => State(s => {
+      val (value, s2) = dice1.run(s)
+      val (tail, s3) = roll1(num - 1).run(s2)
+      val list = value :: tail
+      (list, s3)
+    })
+  }
+
+  // roll implementation based on map2
+  def roll2(num: Int): State[RNG, List[Int]] = num match {
+    case 0 => State(s0 => (Nil, s0)) // recursion exit
+    case _ => dice1.map2(roll2(num-1))((x: Int, xs: List[Int]) => x :: xs)
+  }
+
+  // roll tail recursive implementation
+  def roll3(num: Int): State[RNG, List[Int]] = State( s0 => {
+    @tailrec
+    def go(n: Int, acc: List[Int], s: RNG): List[Int] = n match {
+      case 0 => acc
+      case _ => dice1.run(s) match { case (a, s2) => go(n-1, a::acc, s2) }
+    }
+    (go(num, Nil, s0) reverse, s0)
+  })
+
+  // roll implementation by using scala's foldLeft
+  def roll4(num: Int): State[RNG, List[Int]] = State( s0 => {
+    (1 to num)
+      .foldLeft(
+        (List[Int](), s0)
+      )(
+        (acc, _) => dice1.run(acc._2) match { case (a, s) => (a::acc._1, s) }
+      ) match { case (l, s) => (l reverse, s) }
+  })
+
+
+  val l1 = roll1(10).run(seed)._1
+  val l2 = roll2(10).run(seed)._1
+  val l3 = roll3(10).run(seed)._1
+  val l4 = roll4(10).run(seed)._1
+  println(l1)
+  println(l2)
+  println(l3)
+  println(l4)
+
+  def get[S]: State[S, S] = State( s => (s, s))
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
 
 sealed trait Input
