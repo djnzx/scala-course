26940c97a608f39c4033ab6fed4f8bcd48bbadac
Alexey <alexey.rykhalskiy@gmail.com>
2020-03-27 23:06:13 +0200

    -- cats. p114 done. Writer Monad #2 -- S99: P12-20 - done (mostly recursive and tail-recursive)

--- src/main/scala/cats/C113WriterExercise2.scala
+++ src/main/scala/cats/C113WriterExercise2.scala
@@ -1,57 +1,56 @@
 package cats
 
-import cats.data.{Writer, WriterT}
+import cats.data.Writer
 import cats.instances.vector._
 import cats.syntax.applicative._
-import cats.syntax.writer._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import cats.syntax.writer._      // syntax .tell
 
 object C113WriterExercise2 extends App {
   type Logged[A] = Writer[Vector[String], A]
 
-  // just make everything slowly
-  def slowly[A](body: => A): A =
-    try body finally Thread.sleep(200)
+  val w1: Logged[Unit] = Writer[Vector[String], Unit](implicitly[Monoid[Vector[String]]].empty, ())
+  val w2: Logged[Int]  = w1.map(_ => 1)
+  val w3: Logged[Int]  = w2.mapWritten(v => v.appended("Line: A"))
+  val w4: Logged[Int]  = w3.mapWritten(v => v.appended("Line: B"))
+  val w5: Logged[Unit] = Vector("Line: C").tell
+  val w7: Logged[Int]  = 42.pure[Logged]
+  val sv: Vector[String] = 1 to 10 map { n => s"line: $n!" } toVector
+  val w9: Logged[Unit] = sv.tell
 
-  def myLift1(n: Int): Writer[Vector[String], Int] =
-    Writer[Vector[String], Int](implicitly[Monoid[Vector[String]]].empty, n)
+  /**
+    * .tell and .pure are just implicit syntax
+    *
+    * we can use the value
+    * a <- w2
+    * we can ignore the value
+    * _ <- w2
+    *
+    * all the Vector[String] will be collected
+    *
+    * we cam map the value w.map(val => ...)
+    * we cam map the log   w.mapWritten(log => ...)
+    * we can map both      w.mapBoot((log, val) => ...)
+    * we can map both      w.bimap(log => ..., val => ...)
+    */
+  val wX = for {
+    _ <- w1 // empty/unit
+    a <- w2 // empty + value = 1
+    x <- w4 // A + B + value = 1
+    _ <- w5 // C
+    _ <- Vector("Line: D").tell
+    _ <- Vector("Line: E").tell
+    _ <- w9 // line 1..line 10
+    b <- w7.map(_ + 1000) // empty + value = 1042
+  } yield a + b + x
 
-  val vm = implicitly[Monoid[Vector[String]]]
+  // tell(x) = mapWritten(_ + x)
+  val wY: Logged[Int] = wX.tell(Vector("Finish¡"))
 
-  def myLift2(n: Int): WriterT[Id, Vector[String], Int] =
-    Writer[Vector[String], Int](vm.empty, n)
+  val (log: Vector[String], value: Int)           = wY.run
+  val (log1, value1)                              = wX.run
+  val (log2, value2):       (Vector[String], Int) = wX.run
+  val (log3, value3): Tuple2[Vector[String], Int] = wX.run
 
-  def fact0(n: Int): Int =
-    if (n==0) 1 else fact0(n-1) * n
-
-  def fact1(n: Int): Logged[Int] =
-    for {
-      r <- if (n == 0) 1.pure[Logged]
-           else slowly(fact1(n - 1).map(_ * n))
-      _ <- Vector(s"fact $n = $r").tell
-    } yield r
-
-  def count(): Logged[Int] =
-    for {
-      s <- 1 to 10 map { n => s"line: $n!"}
-      _ <- Vector(s).tell
-    } yield ()
-
-  def fact2(n: Int): Writer[Vector[String], Int] = {
-    val w1 = if (n == 0) 1.pure[Logged] else slowly(fact2(n - 1).map(_ * n))
-    val w2 = w1.flatMap(r => Vector(s"fact $n = $r").tell.map(_ => r))
-    w2
-  }
-
-  val res: Vector[(Vector[String], Int)] = Await.result(Future.sequence(Vector(
-    Future { fact1(5).run },
-    Future { fact2(6).run },
-  )), 10 second)
-  res.foreach(t => {
-    println(s"Log:\n${t._1.mkString("\n")}")
-    println(s"Value:${t._2}")
-  })
+  println(s"log = $log")
+  println(s"val = $value")
 }
---
+++ src/main/scala/cats/C113WriterT.scala
@@ -0,0 +1,21 @@
+package cats
+
+import cats.data.WriterT
+import cats.implicits._
+
+object C113WriterT extends App {
+  // 1. we have data
+  val o_int: Option[Int] = Some(123)
+  // 2. pretend our data[Int] as value for Writer[List[String], Int]
+  // Option[Int] => Writer[List[String], Int] with knowledge Option[...]
+  val writer1: WriterT[Option, List[String], Int] = WriterT.liftF[Option, List[String], Int](o_int)
+  // 3. work with       WriterT[Option, List[String], Int]
+  // as if we work with Writer [        List[String], Int]
+  // but Option...
+  val writer2: WriterT[Option, List[String], Int] = writer1
+    .tell(List("a","b","c"))
+    .tell(List("d","e","f"))
+    .map(x => x + 1)
+  val opt_w: Option[(List[String], Int)] = writer2.run
+  println(opt_w)
+}
