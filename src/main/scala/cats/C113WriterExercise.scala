d782ec519c3259a2105b69e516b934f113966b10
alexr007 <alexey.rykhalskiy@gmail.com>
2020-03-27 13:05:53 +0200

    -- cats. Writer - done -- S99 - P21-P26 done

--- src/main/scala/cats/C113WriterExercise.scala
+++ src/main/scala/cats/C113WriterExercise.scala
@@ -12,24 +12,36 @@ import scala.concurrent.duration._
 object C113WriterExercise extends App {
   type Logged[A] = Writer[Vector[String], A]
 
+  // just make everything slowly
   def slowly[A](body: => A): A =
     try body finally Thread.sleep(200)
 
-  def fact(n: Int): Writer[Vector[String], Int] =
+  def myLift1(n: Int): Writer[Vector[String], Int] =
+    Writer[Vector[String], Int](implicitly[Monoid[Vector[String]]].empty, n)
+
+  val vm = implicitly[Monoid[Vector[String]]]
+
+  def myLift2(n: Int): WriterT[Id, Vector[String], Int] =
+    Writer[Vector[String], Int](vm.empty, n)
+
+  def fact0(n: Int): Int =
+    if (n==0) 1 else fact0(n-1) * n
+
+  def fact1(n: Int): Logged[Int] =
     for {
       r <- if (n == 0) 1.pure[Logged]
-           else slowly(fact(n - 1).map(_ * n))
+           else slowly(fact1(n - 1).map(_ * n))
       _ <- Vector(s"fact $n = $r").tell
     } yield r
 
   def fact2(n: Int): Writer[Vector[String], Int] = {
     val w1 = if (n == 0) 1.pure[Logged] else slowly(fact2(n - 1).map(_ * n))
-    val w2 = w1.flatMap(rx => Vector(s"fact $n = $rx").tell.map(_ => rx))
+    val w2 = w1.flatMap(r => Vector(s"fact $n = $r").tell.map(_ => r))
     w2
   }
 
   val res: Vector[(Vector[String], Int)] = Await.result(Future.sequence(Vector(
-    Future { fact(5).run },
+    Future { fact1(5).run },
     Future { fact2(6).run },
   )), 10 second)
   res.foreach(t => {
