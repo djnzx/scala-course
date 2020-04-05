26c69ac6b8928b0ac05a6ecf34b03d96e66779c3
Alexey <alexey.rykhalskiy@gmail.com>
2020-04-05 14:57:20 +0300

    -- cats. foldable, traversable ... -- partial experiments

--- src/main/scala/cats/C172Traversable.scala
+++ src/main/scala/cats/C172Traversable.scala
@@ -25,24 +25,32 @@ object C172Traversable extends App {
     */
   val zero:    Future[List[Int]] = Future { List.empty[Int] }
 
-  val converted1 = allUptime.foldLeft(zero)((flist, fint) =>
+  val converted1: Future[List[Int]] = allUptime
+    .foldLeft(zero)((flist: Future[List[Int]], fint: Future[Int]) =>
     for {
       list <- flist
       int  <- fint
     } yield list :+ int
   )
+  val r1: List[Int] = Await.result(converted1, 1.seconds)
 
-  val converted2: Future[List[Int]] = Foldable[List].foldLeft(hosts, zero)((flist: Future[List[Int]], host: String) =>
+  val converted2: Future[List[Int]] = Foldable[List].
+    foldLeft(hosts, zero)((flist: Future[List[Int]], host: String) =>
     for {
       list <- flist           // Future[List[Int]]
       int <- getUptime(host) // Future[Int]
     } yield list :+ int      // Future[List[Int] :+ Int]
   )
+  val r2: List[Int] = Await.result(converted2, 10.seconds)
 
+  val converted3: Future[List[Int]] = Future.traverse(hosts)(h => getUptime(h))
+  val r3: List[Int] = Await.result(converted3, 10.seconds)
+
+  println(converted1)
+  println(r1)
   println(converted2)
-  val r: List[Int] = Await.result(converted2, 10.seconds)
-  println(r)
-
-
+  println(r2)
+  println(converted3)
+  println(r3)
 
 }
