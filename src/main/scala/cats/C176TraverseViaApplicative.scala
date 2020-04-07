5c187f01600925d78c133ddde20ca0e0ba414dd5
alexr007 <alexey.rykhalskiy@gmail.com>
2020-04-07 22:55:15 +0300

    -- cats. 177. Traverse in process... -- S99 - P21-P26 done

--- src/main/scala/cats/C176TraverseViaApplicative.scala
+++ src/main/scala/cats/C176TraverseViaApplicative.scala
@@ -1,82 +1,113 @@
 package cats
 
-import cats.instances.list._
-import cats.syntax.applicative._ // pure
-import cats.syntax.apply._       // mapN
-
 object C176TraverseViaApplicative extends App {
 
-  // traverse via traverse
-  def listTraverseT[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-    Traverse[List].traverse(list)(func)
+  /**
+    * direct listTraverse via Traverse[List]traverse
+    */
+  {
+    import cats.instances.list._
 
-  // traverse via applicative
-  def zero[F[_]:Applicative, B]: F[List[B]] = List.empty[B].pure[F]
-
-  def listTraverseA1[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-    list.foldLeft(zero[F, B])((acc, item) => {
-      val fb: F[B] = func(item)
-      val p: F[(List[B], B)] = Apply[F].product(acc, fb)      // tuple them
-      val r: F[List[B]] = Apply[F].map(p)(t => t._1 :+ t._2)  // map to ...
-      r
-    })
-
-  // traverse unfolded implementation
-  def listTraverseA1a[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-    list.foldLeft(zero[F, B])((acc, item) => {
-      // extract instance
-      val af: Apply[F] = Apply[F]
-      val fb: F[B] = func(item)
-      // method syntax
-      def fa (a: List[B])(b: B): (List[B], B) = (a, b)
-      // function syntax
-      val fa2 = (a: List[B]) => (b: B) => (a, b)
-      val ff: F[B => (List[B], B)] = af.map(acc)(fa)
-      val p : F[(List[B], B)] = af.ap(ff)(fb)
-      //
-      val r: F[List[B]] = af.map(p)(t => t._1 :+ t._2)
-      r
-    })
-
-  def listTraverseA3[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-    list.foldLeft(zero[F, B])((acc, item) => {
-      val fb: F[B] = func(item)
-      val r: F[List[B]] = (acc, fb).mapN(_ :+ _)
-      r
-    })
-
-  { //
-    def listTraverseA7[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-      list.foldLeft(zero[F, B])((acc, item) => {
-        // extract instances
-        val functor  = Functor[F]
-        val semigroupal = Semigroupal[F]
-        // apply function
-        val fb: F[B] = func(item)
-        // make product
-        val tupled: F[(List[B], B)] = semigroupal.product(acc, fb)
-        // map
-        val r = functor.map(tupled) { case (a0, a1)  => a0 :+ a1 }
-        r
-      })
+    def listTraverseT[F[_]: Applicative, A, B](list: List[A])(f: A => F[B]): F[List[B]] =
+      Traverse[List].traverse(list)(f)
   }
 
-  { // Semigroupal.map2 direct call
-    def listTraverseA8[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-      list.foldLeft(zero[F, B])((acc, item) => {
-        val fb: F[B] = func(item)
-        val r: F[List[B]] = Semigroupal.map2(acc, fb)(_ :+ _)
-        r
-      })
-  }
+  /**
+    * Traverse via applicative
+    * EMPTY element we need to fold to
+    */
+  import cats.syntax.applicative._ // .pure syntax to lift value
+  def EMPTY[F[_]:Applicative, B]: F[List[B]] = List.empty[B].pure[F]
 
-  { // mapN syntax for Semigroupal
+  /**
+    * Traverse via applicative
+    * mapN syntax for Semigroupal
+    */
+  {
     import cats.syntax.apply._       // mapN
 
-    def listTraverseA9[F[_]: Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
-      list.foldLeft(zero[F, B])((acc, item) => {
-        (acc, func(item)).mapN(_ :+ _)
+    def listTraverseA1[F[_]: Applicative, A, B](list: List[A])(f: A => F[B]): F[List[B]] =
+      list.foldLeft(EMPTY[F, B])((acc, item) => {
+        // apply function A => F[B]
+        val fb: F[B] = f(item)
+        // mapN because F is a functor
+        val r: F[List[B]] = (acc, fb).mapN(_ :+ _)
+        r
       })
   }
+
+  /**
+    * Traverse via applicative
+    * Semigroupal.map2 direct call
+    */
+  def listTraverseA2[F[_]: Applicative, A, B](list: List[A])(f: A => F[B]): F[List[B]] =
+    list.foldLeft(EMPTY[F, B])((acc, item) => {
+      // apply function A => F[B]
+      val fb: F[B] = f(item)
+      // Semigroupal.map2(...)(...)(implicit semigroupal: Semigroupal[F], functor: Functor[F])
+      val r: F[List[B]] = Semigroupal.map2(acc, fb) { combinerFn1 }
+      r
+    })
+
+  /**
+    * Traverse via applicative
+    * Semigroupal.map2 unfolded
+    * tupled tupled: F[(List[B], B)] made by semigroupal.product
+    */
+  def listTraverseA3[F[_]: Applicative, A, B](list: List[A])(f: A => F[B]): F[List[B]] =
+    list.foldLeft(EMPTY[F, B])((acc, item) => {
+      // extract instances
+      val functor: Functor[F] = Functor[F]
+      val semigroupal = Semigroupal.apply[F]
+      // apply function A => F[B]
+      val fb: F[B] = f(item)
+      // make product: def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
+      val tupled: F[(List[B], B)] = semigroupal.product(acc, fb)
+      // map syntax #1. look for syntax #2 in the listTraverseA4
+      val r1 = functor.map(tupled) { t  => t._1 :+ t._2 }
+      r1
+    })
+
+  /**
+    * Traverse via applicative
+    * without semigroupal.product
+    * based only on Applicative and Functor
+    * tupled tupled: F[(List[B], B)] made by Apply[F].ap[A, B](ff: F[A => B])(fa: F[A]): F[B]
+    * actually we describe dependent calculations as partially applied
+    */
+  def combinerFn1[B](lb: List[B], b: B): List[B] = lb :+ b
+  def combinerFn2[B](t: (List[B], B)): List[B] = t._1 :+ t._2
+  def combinerFn3[B](t: (List[B], B)): List[B] = t match { case (lb, b)  => lb :+ b }
+
+  def listTraverseA4[F[_]: Applicative, A, B](list: List[A])(f: A => F[B])(
+    implicit functor: Functor[F], af: Apply[F]
+  ): F[List[B]] =
+    list.foldLeft(EMPTY[F, B])((acc: F[List[B]], item) => {
+      // apply function A => F[B]
+      val fb: F[B] = f(item)
+      /**
+        * the task is to join `F[List[B]] (acc)` and F[B] (fb) without flatMap
+        */
+      val product_curry: List[B] => B => (List[B], B) =
+        lb => b => (lb, b)
+
+      val partially_applied: F[B => (List[B], B)] = functor.map(acc)     { product_curry }
+      //                                                     F[List[B]]  lb => b => (lb, b)
+      val tupled : F[(List[B], B)] = af.ap(partially_applied) { fb }
+      //                                 F[B => (List[B], B)]  F[B]
+
+      // map syntax #2. look for syntax #1 in the listTraverseA3
+      val r2: F[List[B]] = functor.map(tupled) { combinerFn3 }
+      r2
+    })
+
+  def listTraverseA5[F[_]: Applicative, A, B](list: List[A])(f: A => F[B])(implicit af: Apply[F]): F[List[B]] =
+    list.foldLeft(EMPTY[F, B])((acc, item) =>
+      af.map(
+        af.ap(
+          af.map(acc) { lb => (b: B) => (lb, b) }
+        ) { f(item) }
+      ) { combinerFn3 }
+    )
 
 }
