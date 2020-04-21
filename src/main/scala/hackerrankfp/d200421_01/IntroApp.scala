ee84235e12b5a6574b21fdfe8d4ee5dd54f20456
alexr007 <alexey.rykhalskiy@gmail.com>
2020-04-21 17:22:50 +0300

    -- HR/FP: Points: 73 Rank: 18364

--- src/main/scala/hackerrankfp/d200421_01/IntroApp.scala
+++ src/main/scala/hackerrankfp/d200421_01/IntroApp.scala
@@ -20,5 +20,19 @@ object IntroApp extends App {
   def readInt = scala.io.Source.stdin.getLines().take(1).map(_.toInt).sum
   println(f02a(readInt))
 
+  /**
+    * https://www.hackerrank.com/challenges/fp-reverse-a-list/problem?h_r=next-challenge&h_v=zen
+    */
+  def f03(arr:List[Int]):List[Int] = {
+    def fr(xs: List[Int], acc: List[Int]): List[Int] = xs match {
+      case h::t => fr(t, h::acc)
+      case _ => acc
+    }
+    fr(arr, List.empty[Int])
+  }
 
+  /**
+    * https://www.hackerrank.com/challenges/fp-sum-of-odd-elements/problem?h_r=next-challenge&h_v=zen
+    */
+  def f(arr:List[Int]):Int = arr.foldLeft(0) { (acc, el) => acc + (if (el %2 != 0) el else 0)}
 }
