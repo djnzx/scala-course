4a35545c92e06ab4e0948c10734f4ddd8ac48fcd
Alexey <alexey.rykhalskiy@gmail.com>
2020-04-21 22:30:30 +0300

    -- HR/FP: Points: 148 Rank: 8199 15% - polished

--- src/main/scala/hackerrankfp/d200421_01/Easy1App.scala
+++ src/main/scala/hackerrankfp/d200421_01/Easy1App.scala
@@ -7,6 +7,9 @@ object Easy1App extends App {
 
   def readLine = scala.io.StdIn.readLine()
 
+  val STEP = 0.001
+  val STEPW = (1/STEP).toInt
+
   def pow(x: Double, n: Int): Double = {
     val v = (1 to scala.math.abs(n)).foldLeft(1: Double)((acc, _) => acc * x)
     if      (n > 0) v
@@ -14,27 +17,28 @@ object Easy1App extends App {
     else 1
   }
 
-  def f(coefficients:List[Int], powers: List[Int], x:Double): Double =
-    (coefficients zip powers)
-      .foldLeft(0: Double) { (acc, el) => acc + el._1*pow(x, el._2)*0.001 }
+  val square = (v: Double) => v*STEP
+  val volume = (v: Double) => pow(v, 2)*scala.math.Pi*STEP
 
-  def area(coefficients:List[Int], powers: List[Int], x:Double): Double =
-    pow(f(coefficients, powers, x)*1000, 2)*scala.math.Pi/1000
+  def valueAt(ks:List[Int], ps: List[Int], x:Double)(fx: Double => Double): Double =
+    fx((ks zip ps).foldLeft(0: Double) { (acc, el) => acc + el._1*pow(x, el._2) })
+
+  def f(ks:List[Int], ps: List[Int], x:Double): Double =
+    valueAt(ks, ps, x)(square)
+
+  def area(ks:List[Int], ps: List[Int], x:Double): Double =
+    valueAt(ks, ps, x)(volume)
 
   def range(l: Int, r: Int): Seq[Double] =
-    (l*1000 to r*1000).map(_.toDouble/1000).toVector
+    (l*STEPW to r*STEPW).map(_.toDouble*STEP).toVector
 
   def summation(func: (List[Int], List[Int], Double) => Double,
-                 upperLimit: Int, lowerLimit: Int,
-                 coefficients: List[Int], powers: List[Int]
-               ): Double = {
-    val minmax: Seq[Double] = range(lowerLimit, upperLimit)
-    minmax.foldLeft(0: Double) { (acc, x) => acc + func(coefficients, powers, x) }
-  }
+                upperLimit: Int, lowerLimit: Int,
+                coefficients: List[Int], powers: List[Int]): Double =
+    range(lowerLimit, upperLimit).foldLeft(0: Double) { (acc, x) => acc + func(coefficients, powers, x) }
 
   val r1 = summation(   f, 20, 2, List(1,2), List(0,1) )
   val r2 = summation(area, 20, 2, List(1,2), List(0,1) )
   println(r1)
   println(r2)
-
 }
