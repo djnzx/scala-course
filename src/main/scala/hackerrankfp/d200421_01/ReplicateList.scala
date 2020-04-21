d05d0d5d5a0940eeafba49268b05cb7d8b675597
alexr007 <alexey.rykhalskiy@gmail.com>
2020-04-21 17:03:35 +0300

    -- HR/FP: Points: 33 Rank: 23082

---
+++ src/main/scala/hackerrankfp/d200421_01/ReplicateList.scala
@@ -0,0 +1,11 @@
+package hackerrankfp.d200421_01
+
+/**
+  * https://www.hackerrank.com/challenges/fp-list-replication/problem
+  */
+object ReplicateList extends App {
+
+  def f(num:Int,arr:List[Int]):List[Int] =
+    arr.flatMap(x => 1 to num map { _ => x })
+
+}
