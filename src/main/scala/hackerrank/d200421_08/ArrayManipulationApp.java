e28a8043bd478a13a3235db31461aaec8c85447c
Alexey <alexey.rykhalskiy@gmail.com>
2020-04-23 19:20:24 +0300

    -- HR/FP: Points: 280 Rank: 2998 Page: 150

--- src/main/scala/hackerrank/d200421_08/ArrayManipulationApp.java
+++ src/main/scala/hackerrank/d200421_08/ArrayManipulationApp.java
@@ -6,17 +6,17 @@ package hackerrank.d200421_08;
 public class ArrayManipulationApp {
 
   static long arrayManipulation(int n, int[][] queries) {
-    int max = 0;
-    int []process = new int[n+1];
+    long []process = new long[n+1];
     for (int[] query : queries) {
       process[query[0] - 1] += query[2];
-      process[query[1]] -= query[2];
+      process[query[1]    ] -= query[2];
     }
 
-    int temp = 0;
-    for (int value : process) {
+    long max = 0;
+    long temp = 0;
+    for (long value: process) {
       temp += value;
-      if (temp > max) max = temp;
+      max = Math.max(max, temp);
     }
     return  max;
   }
