282fea5ac9bba06cdd2d66576500701219931a5d
alexr007 <alexey.rykhalskiy@gmail.com>
2020-04-27 09:15:23 +0300

    -- Apple And Oranges

--- src/main/scala/hackerrank/d200319_04/Warmups.java
+++ src/main/scala/hackerrank/d200319_04/Warmups.java
@@ -4,7 +4,10 @@ import java.time.LocalTime;
 import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -127,10 +130,46 @@ public class Warmups {
     ).collect(toList());
   }
 
-  static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+  /**
+   * https://www.hackerrank.com/challenges/apple-and-orange/problem
+   */
+  static void countApplesAndOranges1(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    long ac = Arrays.stream(apples).map(x -> x + a).filter(p -> p >= s && p <= t).count();
+    long oc = Arrays.stream(oranges).map(x -> x + b).filter(p -> p >= s && p <= t).count();
+    System.out.printf("%d\n%d\n", ac, oc);
+  }
+
+  static void countApplesAndOranges2(int s, int t, int a, int b, int[] apples, int[] oranges) {
     IntPredicate ip = p -> p >= s && p <= t;
-    long ac = Arrays.stream(apples).map(a1 -> a + a1).filter(ip).count();
-    long oc = Arrays.stream(oranges).map(o1 -> b + o1).filter(ip).count();
+    long ac = Arrays.stream(apples).map(x -> x + a).filter(ip).count();
+    long oc = Arrays.stream(oranges).map(x -> x + b).filter(ip).count();
+    System.out.printf("%d\n%d\n", ac, oc);
+  }
+
+  static void countApplesAndOranges3(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    IntPredicate ip = p -> p >= s && p <= t;
+    IntUnaryOperator plusa = x -> x + a;
+    IntUnaryOperator plusb = x -> x + b;
+    long ac = Arrays.stream(apples).map(plusa).filter(ip).count();
+    long oc = Arrays.stream(oranges).map(plusb).filter(ip).count();
+    System.out.printf("%d\n%d\n", ac, oc);
+  }
+
+  static void countApplesAndOranges4(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    IntPredicate ip = p -> p >= s && p <= t;
+    Function<Integer, IntUnaryOperator> plus = y -> x -> x + y;
+    long ac = Arrays.stream(apples).map(plus.apply(a)).filter(ip).count();
+    long oc = Arrays.stream(oranges).map(plus.apply(b)).filter(ip).count();
+    System.out.printf("%d\n%d\n", ac, oc);
+  }
+
+  static void countApplesAndOranges5(int s, int t, int a, int b, int[] apples, int[] oranges) {
+    IntPredicate ip = p -> p >= s && p <= t;
+    Function<Integer, IntUnaryOperator> plus = y -> x -> x + y;
+    BiFunction<int[], IntUnaryOperator, Long> count =
+        (ints, f) -> Arrays.stream(ints).map(f).filter(ip).count();
+    long ac = count.apply(apples, plus.apply(a));
+    long oc = count.apply(oranges, plus.apply(b));
     System.out.printf("%d\n%d\n", ac, oc);
   }
 
