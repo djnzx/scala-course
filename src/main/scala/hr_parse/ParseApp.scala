ce5da6784f838177e84b2b9fbea12da62ee12b4f
alexr007 <alexey.rykhalskiy@gmail.com>
2020-05-14 10:40:17 +0300

    -- jsoup HR parser

--- src/main/scala/hr_parse/ParseApp.scala
+++ src/main/scala/hr_parse/ParseApp.scala
@@ -1,40 +1,48 @@
 package hr_parse
 
+import org.http4s.Uri
 import org.jsoup._
 import org.jsoup.select.Elements
 import org.http4s.implicits._
 
+/**
+  * Parser for HackerRank scores
+  * https://www.hackerrank.com/leaderboard?filter=elgun_cumayev&filter_on=hacker&page=1&track=algorithms&type=practice
+  */
 object ParseApp extends App {
 
   case class HackerDetails(name: String, rank: Int, country: String, score: Double)
 
-  val elementToHacker = (el: Elements) => {
-    val hacker = el.select(".hacker a").attr("data-value")
-    val rank = el.select(".rank div").text
+  // TODO put validation here and make return type Either[String, HackerDetails]
+  val toHacker = (el: Elements) => {
+    val hacker  = el.select(".hacker a").attr("data-value")
+    val rank    = el.select(".rank div").text
     val country = el.select(".extra .flag-tooltip").attr("data-balloon")
-    val score = el.select(".score div").text
+    val score   = el.select(".score div").text
     HackerDetails(hacker, rank.toInt, country, score.toDouble)
   }
 
-  val linkToElement = (link: String) => {
-    val doc = Jsoup.connect(link).get
-    doc.select(".ui-leaderboard-table .table-body .table-row")
-  }
+  val toElement = (uri: Uri) =>
+    Jsoup.connect(uri renderString).get
+      .select(".ui-leaderboard-table .table-body .table-row")
 
-  val buildUri: (String, String) => String = (name: String, topic: String) =>
-    uri"https://www.hackerrank.com" / "leaderboard" +?
-      ("filter", name) +? ("filter_on", "hacker") +? ("track", topic) +? ("type", "practice") renderString
+  val buildUri = (topic: String) => (name: String) =>
+    uri"https://www.hackerrank.com" / "leaderboard" withQueryParams Map(
+      "filter" -> name,
+      "filter_on" -> "hacker",
+      "track" -> topic,
+      "type" -> "practice"
+    )
 
-  /**
-    * https://www.hackerrank.com/leaderboard?filter=elgun_cumayev&filter_on=hacker&page=1&track=algorithms&type=practice
-    */
-  val requestWithTopic = (user: String, topic: String) =>  elementToHacker(linkToElement(buildUri(user, topic)))
-
-  val names = Vector("elgun_cumayev", "alexr007")
   val topic = "algorithms"
+  val names = Vector("elgun_cumayev", "alexr007", "ibcelal", "lcavadova", "realserxanbeyli")
 
-  val request = (user: String) => requestWithTopic(user, topic)
+  val rqWithTopic = buildUri(topic)
 
-  val results: Vector[HackerDetails] = names.map(request)
-  results foreach println
+  names
+    .map { rqWithTopic }
+    .map { toElement }
+    .map { toHacker }
+    .sortBy { _.rank }
+    .foreach { println }
 }
