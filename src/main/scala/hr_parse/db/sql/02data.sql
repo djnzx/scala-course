68e9c79570f6f73fb308df1188a25d68ab84f8dd
alexr007 <alexey.rykhalskiy@gmail.com>
2020-05-14 20:43:10 +0300

    meta

---
+++ src/main/scala/hr_parse/db/sql/02data.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.groups (id, name) VALUES (1, 'BE1');
+INSERT INTO public.groups (id, name) VALUES (2, 'BE2');
+INSERT INTO public.groups (id, name) VALUES (3, 'BE3');
+INSERT INTO public.groups (id, name) VALUES (4, 'BE4');
+
+INSERT INTO public.students (id, name, hacker, groupp) VALUES (1, 'Adil/Aqil Abdurrazaklı', 'abdurrazakliadil', 4);
+INSERT INTO public.students (id, name, hacker, groupp) VALUES (2, 'Aflatun Velibeyli', 'a_velibeyli6', 4);
+INSERT INTO public.students (id, name, hacker, groupp) VALUES (3, 'Intigam Nagiyev', 'nagiyevintiqam', 4);
+INSERT INTO public.students (id, name, hacker, groupp) VALUES (4, 'Fidan Ismayilova', 'fidan97ismailova', 4);
+INSERT INTO public.students (id, name, hacker, groupp) VALUES (5, 'Jalal Ibrahimkhalilov', 'ibcelal', 4);
+
+INSERT INTO public.ranks (id, date, student, rank, score) VALUES (3, '2020-05-14 13:18:59.995967', 1, 1000, 50.6);
+INSERT INTO public.ranks (id, date, student, rank, score) VALUES (4, '2020-05-14 13:19:55.605450', 1, 990, 51.5);
