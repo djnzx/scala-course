2a780cab62eb04bfa9fc834f0cdcb614dd49c013
Alexey <alexey.rykhalskiy@gmail.com>
2020-05-15 23:22:41 +0300

    -- one more task in the process

--- src/main/scala/hackerrank/d200515_09/SpecialStringAgainApp.scala
+++ src/main/scala/hackerrank/d200515_09/SpecialStringAgainApp.scala
@@ -5,49 +5,27 @@ package hackerrank.d200515_09
   * 14/17 test cases failed :(
   */
 object SpecialStringAgainApp extends App {
-
-  sealed trait Itm
-  final case class Item(c: Char, cnt: Int) extends Itm
-  final case object Empty extends Itm
-  final case class Res(l: List[Item], max: Int, buf: Itm)
-
+  val oneToN = (n: Long) => n*(n+1)/2
+  final case class Item(c: Char, cnt: Long)
+  final case class Res(l: List[Item], buf: List[Char])
   def substrCount(n: Int, origin: String): Long = {
-    val rx: Res = origin.foldLeft(Res(Nil, 0, Empty)) { (r, c) => r match {
-      case Res(list, max, Empty)         => Res(list, max, Item(c, 1))
-      case Res(list, max, Item(ch, cnt)) =>
-        if (c==ch)                               Res(list, max, Item(c, cnt+1))
-        else      Res(Item(ch, cnt)::list, math.max(max, cnt), Item(c, 1))
+    val rx: Res = origin.foldLeft(Res(Nil, Nil)) { (r, c) => r match {
+      case Res(list, Nil)         => Res(list, c::Nil)
+      case Res(list, h::t)       =>
+        if (c==h)                    Res(list, c::h::t)
+        else                         Res(Item(h, r.buf.size)::list, c::Nil)
     }}
-    val (ls, max) = (rx.buf::rx.l reverse, math.max(rx.max, rx.buf.asInstanceOf[Item].cnt))
-      println(s"ls: $ls")
-      println(s"max: $max")
-
-    def calcOneToN(n: Int, acc: List[Int]): Vector[Int] =
-      if      (n == 1  ) calcOneToN(n+1, List(1))
-      else if (n <= max) calcOneToN(n+1, n+acc.head::acc)
-      else 0::(acc reverse) toVector
-
-    val oneToN = calcOneToN(1, Nil)
-      println(s"oneToN: $oneToN")
-
-    def fold(tl: List[Item], acc: Int): Int = {
-//      println(s"$tl : $acc")
-      tl match {
-        case Nil           => acc
-        case h::Nil        => fold(Nil,    acc + oneToN(h.cnt))
-        case h::t::Nil     => fold(t::Nil, acc + oneToN(h.cnt))
-        case l::c::r::tail =>
+    val ls = Item(rx.buf.head, rx.buf.size)::rx.l
+    def fold(tl: List[Item], acc: Long): Long = tl match {
+        case Nil             => acc
+        case h::Nil          => fold(Nil,    acc + oneToN(h.cnt))
+        case h::t::Nil       => fold(t::Nil, acc + oneToN(h.cnt))
+        case l::c::r::tail   =>
           if (l==r && c.cnt==1) fold(c::r::tail, acc + oneToN(l.cnt) + l.cnt)
           else                  fold(c::r::tail, acc + oneToN(l.cnt))
       }
-    }
-
-    val n = fold(ls.asInstanceOf[List[Item]], 0)
-    println(n)
-    n
+    fold(ls, 0)
   }
-
-  val s = "abcbaba"
-  substrCount(s.length, s)
-
+  val s = "asasd"
+  println(substrCount(s.length, s))
 }
