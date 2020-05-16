cb7bd4e3e583b0e774e1da438775579885630f05
alexr007 <alexey.rykhalskiy@gmail.com>
2020-05-16 20:50:55 +0300

    one more task

--- src/main/scala/hackerrank/d200515_09/CountTripletsApp.scala
+++ src/main/scala/hackerrank/d200515_09/CountTripletsApp.scala
@@ -2,21 +2,27 @@ package hackerrank.d200515_09
 
 /**
   * https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
-  * doesn't work
+  * 8 of 13 Your code did not execute within the time limits
   */
 object CountTripletsApp extends App {
 
   def countTriplets(a: Array[Long], r: Long): Long = {
     val len = a.length
-
-    (0 to len-3).flatMap { i =>
-      (i to len-2).flatMap { j =>
-        (j to len-1).map { k =>
-          (i,j,k)
-        }
+    (0 to len-3)
+      .map { i => (i, a(i)*r) }
+      .flatMap { case (i, x) =>
+        (i+1 to len-2)
+          .filter { j => a(j) == x }
+          .map { j => (j, a(j)*r) }
+          .flatMap { case (j, y) =>
+            (j+1 to len-1)
+              .filter { k => a(k) == y }
+              .map { k =>
+                (i,j,k)
+              }
+          }
       }
-    }
-      .count { case (i,j,k) => val x = a(i)*r; a(j) == x && a(k) == x*r }
+      .length
   }
   // exp: 161700
   // fact: 171598
