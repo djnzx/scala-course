36ef600daac5ca530ddfead20e3c85c1854ae948
alexr007 <alexey.rykhalskiy@gmail.com>
2020-05-16 22:32:33 +0300

    one more task still n^3

--- src/main/scala/hackerrank/d200515_09/CountTripletsApp.scala
+++ src/main/scala/hackerrank/d200515_09/CountTripletsApp.scala
@@ -6,7 +6,7 @@ package hackerrank.d200515_09
   */
 object CountTripletsApp extends App {
 
-  def countTriplets(a: Array[Long], r: Long): Long = {
+  def countTripletsNaive(a: Array[Long], r: Long): Long = {
     val len = a.length
     (0 to len-3)
       .map { i => (i, a(i)*r) }
@@ -21,7 +21,28 @@ object CountTripletsApp extends App {
       }
       .length
   }
-  // exp: 161700
-  // fact: 171598
+
+  case class IV(i: Int, v: Long)
+  def countTriplets(a: Array[Long], r: Long): Long = {
+    val ai = a.zipWithIndex.map { case (v, i) => IV(i,v) }.toVector
+    var cnt = 0L
+    (0 to a.length-3).foreach { i =>
+      val x1 = a(i)*r
+      val x2 = x1*r
+
+      val x1x2: Vector[IV] = ai  .filter { case IV(idx, v) => v == x1 || v == x2 && idx > i }
+      val ax1:  Vector[IV] = x1x2.filter { case IV(idx, v) => v == x1            && idx > i }
+
+      cnt +=
+        ax1.flatMap { iv1 =>
+          x1x2
+            .filter { case IV(idx, v) => v == x2 && idx > iv1.i }
+        }
+          .length
+
+    }
+    cnt
+  }
+  // 161700
   println(countTriplets(Array.fill[Long](100)(1),1))
 }
