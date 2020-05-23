0c33e130ae787b2cc5cbc096ff7e653e91d83e71
alexr007 <alexey.rykhalskiy@gmail.com>
2020-05-23 23:14:15 +0300

    -- small fix

--- src/main/scala/catseffx/timed_input/TimedInputApp5.scala
+++ src/main/scala/catseffx/timed_input/TimedInputApp5.scala
@@ -7,7 +7,7 @@ import scala.io.StdIn
 object TimedInputApp5 extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = for {
-    _ <- IO(println("Enter value:")) *> IO(1)
+    _ <- IO(println("Enter value:"))
     x <- Concurrent.timeoutTo(IO(StdIn.readLine), 5.seconds, IO("555"))
     _ <- IO(println(s"Value entered: $x"))
   } yield ExitCode.Success
