53a2e19b470d276d9097c951dbc92ba6b2c3f155
alexr007 <alexey.rykhalskiy@gmail.com>
2020-06-03 21:43:17 +0300

    -- HR FP 448

--- scala_plain/src/main/scala/hackerrankfp/d200602_08/PrisonTransportV4.scala
+++ scala_plain/src/main/scala/hackerrankfp/d200602_08/PrisonTransportV4.scala
@@ -1,64 +1,98 @@
 package hackerrankfp.d200602_08
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * https://www.hackerrank.com/challenges/prison-transport/problem
   * doesn't pass one test
   */
 object PrisonTransportV4 {
+  type MMap[A,B] = mutable.Map[A,B]
+  type MSet[A] = mutable.Set[A]
+  case class Hold(ids: ArrayBuffer[Int], cnt: ArrayBuffer[Int], 
+                  set: MMap[Int, MSet[Int]] = mutable.Map.empty, 
+                  join: ArrayBuffer[(Int, Int)] = ArrayBuffer.empty) {
+    def print = {
+      println(s"ids: $ids")
+      println(s"cnt: $cnt")
+      println(s"set: $set")
+    }
+  }
   // pricing
   def priceOneGroup(n: Int): Int = math.ceil(math.sqrt(n)).toInt
   def priceAllGroups(xs: Iterable[Int]): Int = xs.toVector.map { priceOneGroup }.sum
   def priceTotal(N: Int, xs: Iterable[Int]): Int = N - xs.sum + priceAllGroups(xs)
-  
-  // processing
-  import scala.collection.mutable
-  type MMap[A,B] = mutable.Map[A,B]
-  type MSet[A] = mutable.Set[A]
+  var i = 0
+  def processPair(a: Int, b: Int, h: Hold): Unit = {
+//    println(i)
+//    i += 1
+    (h.ids(a), h.ids(b)) match {
+      case (0, 0) =>
+//        println(s"A: $a $b")
+        // point to group
+        val gid = a
+        h.ids(a) = gid
+        h.ids(b) = gid
+        // count the group elements
+        h.cnt(gid) = 2
+        // put the keys
+        h.set.addOne(gid -> mutable.Set(a, b))
 
-  val m: MMap[Int, Int] = mutable.Map.empty
-  val mapset: MMap[Int, MSet[Int]]= mutable.Map.empty
-  
-  def processPair(a: Int, b: Int): Unit = {
-    (m.contains(a), m.contains(b)) match {
-      case (false, false) =>
-        m.addAll(Seq(a -> a, b -> a))       // set_id = a
-        mapset.addOne(a -> mutable.Set(a, b)) // set_id -> set_contents
+      case (gida, 0) =>
+//        println(s"B: $a $b")
+        // point to group
+        h.ids(b) = gida
+        // increment count
+        h.cnt(gida) += 1
+        // add the element
+        h.set(gida).add(b)
 
-      case (true,  false) =>
-        val set_id = m(a)
-        m.addOne(b -> set_id)
-        mapset(set_id).addOne(b)
-
-      case (false, true) =>
-        val set_id = m(b)
-        m.addOne(a -> set_id)
-        mapset(set_id).addOne(a)
+      case (0, gidb) =>
+//        println(s"C: $a $b")
+        // point to group
+        h.ids(a) = gidb
+        // increment count
+        h.cnt(gidb) += 1
+        // add the element
+        h.set(gidb).add(a)
         
-      case (true,  true ) => 
-        val set_id1 = m(a)
-        val set_id2 = m(b)
-        val set1 = mapset(set_id1)
-        val set2 = mapset(set_id2)
-        set1.addAll(set2)
-        set2.foreach { k => m.put(k, set_id1) }
+      case (gida, gidb) =>
+        println(s"D: $a $b")
+        // set a += set b
+        println("D0")
+        println(s"cnta: ${h.cnt(a)}, cntb: ${h.cnt(b)}")
+        println(s"gida: $gida, gidb: $gidb")
+        val seta: Option[MSet[Int]] = h.set.get(a)
+        val setb: Option[MSet[Int]] = h.set.get(b)
+        println(seta)
+        println(setb)
+        h.set(gida).addAll(h.set(gidb))
+        println("D1")
+        // add counter
+        h.cnt(gida) += h.cnt(gidb)
+        println("D2")
+        // clear 2nd counter
+        h.cnt(gidb) = 0
+        println("D3")
+        // rewrite group number
+        h.set(gidb).foreach(k => h.ids(k) = gida)
+        println("D4")
+        // remove group b
+        h.set.remove(gidb)
+        println("D5")
     }
   }
   
-  def simplify(m: MMap[Int, MSet[Int]]): Iterable[Int] = {
-    val a: Iterable[MSet[Int]] = m.values
-    val b: Set[MSet[Int]] = a.toSet
-    val c: Set[Int] = b.map { _.size }
-    c
-  }
-
+  def simplify(cnt: ArrayBuffer[Int]): Iterable[Int] = cnt.toVector.filter(_ > 0)
+  
   def process(N: Int, pairs: Seq[(Int, Int)]): Int = {
-    pairs.foreach { case (a, b) => processPair(a, b) }
-    println(m)
-//    val groups = simplify(m)
-//    println(groups)
-//    println(s"N = ${N}")
-//    priceTotal(N, groups)
-    -1
+    val h = Hold(ArrayBuffer.fill[Int](N+1)(0), ArrayBuffer.fill[Int](N+1)(0))
+    pairs.foreach { case (a, b) => processPair(a, b, h) }
+    val groups = simplify(h.cnt)
+    h.print
+    println(s"g:$groups")
+    priceTotal(N, groups)
   }
 
   def body(line: => String): Unit = {
