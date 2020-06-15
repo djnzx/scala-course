cf87c3a3b07683f6ecbdf1606933e6319cdc7f65
alexr007 <alexey.rykhalskiy@gmail.com>
2020-06-15 06:10:19 +0300

    -- SimplifyAlgebraicExpressions -- some tests for JsonParser

--- fp_red/src/main/scala/fp_red/red09/JSONParserSpec.scala
+++ fp_red/src/main/scala/fp_red/red09/JSONParserSpec.scala
@@ -1,12 +1,10 @@
 package fp_red.red09
 
-import fp_red.red09.JSON.{JArray, JBool, JNumber, JObject, JString}
-import org.scalatest.{EitherValues, Inside, Inspectors, OptionValues, funspec, matchers}
+import fp_red.red09.JSON._
+import org.scalatest.{Inside, Inspectors, funspec, matchers}
 
 class JSONParserSpec extends funspec.AnyFunSpec
   with matchers.should.Matchers
-  with OptionValues
-  with EitherValues
   with Inside
   with Inspectors {
 
@@ -14,9 +12,22 @@ class JSONParserSpec extends funspec.AnyFunSpec
   val json = JSON.jsonParser(P)
   val parser: String => Either[ParseError, JSON] = P.run(json)
   
-  describe("number") {
+  describe("JSON") {
+    it("object with one number field") {
+      val text = """{ "a" : 123 }"""
 
-    val jsonTxt = """
+      parser(text) shouldEqual Right(JObject(Map("a" -> JNumber(123))))
+    }
+
+    it("array of integers") {
+      val text = """{ "a" : [1,2,3] }"""
+
+      parser(text) shouldEqual Right(JObject(Map("a" -> JArray(IndexedSeq(JNumber(1),JNumber(2),JNumber(3))))))
+    }
+
+    it("complete object") {
+
+      val text = """
 {
   "Company name" : "Microsoft Corporation",
   "Ticker"  : "MSFT",
@@ -27,7 +38,12 @@ class JSONParserSpec extends funspec.AnyFunSpec
 }
 """
 
-    val r = parser(jsonTxt)
-    r.right.value should beJObject(Map("Shares outstanding" -> JNumber(8.38E9), "Price" -> JNumber(30.66), "Company name" -> JString("Microsoft Corporation"), "Related companies" -> JArray(Vector(JString("HPQ"), JString("IBM"), JString("YHOO"), JString("DELL"), JString("GOOG"))), "Ticker" -> JString("MSFT"), "Active" -> JBool(true)))
+      parser(text) shouldEqual
+        Right(
+          JObject(Map("Shares outstanding" -> JNumber(8.38E9), "Price" -> JNumber(30.66), "Company name" -> JString("Microsoft Corporation"), "Related companies" -> JArray(Vector(JString("HPQ"), JString("IBM"), JString("YHOO"), JString("DELL"), JString("GOOG"))), "Ticker" -> JString("MSFT"), "Active" -> JBool(true)))
+        )
+    }
+
   }
+  
 }
