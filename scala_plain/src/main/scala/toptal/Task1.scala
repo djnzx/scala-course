db8b672d604fcd279151d8456d1872b1d694cc08
Alexey <alexey.rykhalskiy@gmail.com>
2020-08-15 12:30:01 +0300

    TopTal1

--- scala_plain/src/main/scala/toptal/Task1.scala
+++ scala_plain/src/main/scala/toptal/Task1.scala
@@ -4,20 +4,19 @@ object Task1 {
 
   def solution(s: String, x: Array[Int], y: Array[Int]): Int =
     (s zip (x zip y).map { case (x, y) => x * x + y * y })
-      .sortBy(_._2)
-      // and right here we need collect just to first duplicate letter
-      .groupBy(_._2)
+      .groupMap { case (_, r) => r } { case (ch, _) => ch }
       .toVector
-      .map { case (r, sq) => (r, sq.map(_._1)) }
-      .sortBy(_._1)
-      .takeWhile { case (_, sq) => sq.length == 1 }
-      .foldLeft((0, Map.empty[Char, Long])) { case ((mx, map), (_, sqc)) =>
-        val map2: Map[Char, Long] = sqc.foldLeft(map) { (m, l) => m.updated(l, m.getOrElse(l, 0L) + 1L) }
-        val max2 = mx max map2.count { case (_, v) => v == 1 }
-        (max2, map2)
+      .sortBy { case (r, _) => r }
+      .view
+      .map { case (_, cs) => cs }
+      .takeWhile { cs => cs.size == cs.toSet.size }
+      .foldLeft((false, Set.empty[Char])) {
+        case ((done, set), chars) =>
+          if (!done && chars.forall(!set.contains(_))) {
+            (false, set ++ chars)
+          } else {
+            (true, set)
+          }
       }
-      ._1
-  
-  
-  
+    match { case (_, set) => set.size }
 }
