302fbf5d00608cbbe44c14057ed2102a8d6505b2
alexr007 <alexey.rykhalskiy@gmail.com>
2020-08-29 08:49:57 +0300

    coProduct

--- lihaoyi/src/main/scala/request/ActApi.scala
+++ lihaoyi/src/main/scala/request/ActApi.scala
@@ -1,27 +1,51 @@
 package request
 
-import request.dto.RegRq
-import requests.{Response, connectTimeout}
+import request.dto.{LoginRq, RegRq}
+import requests.Response
 
 object ActApi extends App {
   val urlBase    = "http://aacctt.ddns.net:19901"
   val urlAuth    = s"$urlBase/auth-service"
-  val urlAuthReg = s"$urlAuth/ss/account/register"
-  // POST
-  val regRq = RegRq("a77x771@gmail.com", "alexr1", "r", "123-45-67", "M", "iOS13", "121212", "A", "B", "abc123DEF")
-  /** Serialization: Case Class => String */
-  val json: String = upickle.default.write(regRq)
-  println(urlAuthReg)
-  println(regRq)
-  println(json)
-
-  pprint.log(json)
-  
-  val r: Response = requests.post(urlAuthReg, headers = Seq(
+  val headers = Seq(
     "Content-Type"->"application/json",
-//    "Accept"->"*/*",
-//    "Accept-Encoding"->"gzip, deflate, br"
-  ), data = json)
-  pprint.log(r)
+    //    "Accept"->"*/*",
+    //    "Accept-Encoding"->"gzip, deflate, br"
+  )
+  
+  val email = "a77x77@gmail.com"
+  val password = "abc123DEF"
+  
+  def registerNewUser = {
+    val urlAuthReg = s"$urlAuth/ss/account/register"
+    val regRq = RegRq(email, "alexr1", "r", "123-45-67", "M", "iOS13", "121212", "A", "B", password)
+    val json: String = upickle.default.write(regRq)
+    val r: Response = requests.post(urlAuthReg, headers = headers, data = json)
+    pprint.log(r)
+    /**
+      * "act_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3MDUiLCJleHAiOjE1OTg0Nzc5NDEsImlhdCI6MTU5ODQ3NDM0MX0.75JRrMi-v8ZM75dO8C6oSeX-FmbI7Q9BlsbufnO2_CQ",
+      * "user_Id": "0a68781a-792a-427c-8f7a-12171966a807",
+      * "ep_enabled": false,
+      * "refresh_token": "M1lSzZawwP3frINhysgEN0qnmkqpBhNK85hoioy0yqphLCCfj1dhSTn3yaWWfhIyemcLb1raD4zSmqrBQvsnwuyXLf1SGEz0lDKQS5SIFnXEiFTXMcDYC0su4zey9smb"
+      */
+  }
+  
+  def login = {
+    val urlLogin   = s"$urlAuth/ss/account/login"
+    val loginRq = LoginRq(email, password)
+    val json = upickle.default.write(loginRq)
+    val r: Response = requests.post(urlLogin, headers = headers, data = json)
+    pprint.log(r)
+    /**
+      * {
+      *   "act_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3MDUiLCJleHAiOjE1OTg1NjIyNDksImlhdCI6MTU5ODU1ODY0OX0.gUf7jsegnwDMS-Z79XimqksnibxKezAjr8do4Zny6Zs",
+      *   "user_Id":"0a68781a-792a-427c-8f7a-12171966a807",
+      *   "ep_enabled":false,
+      *   "refresh_token":"FHpo0en9e0up32MO6vOnIDGOEmyVzBXtMscSMTePCKraW9DhGkL3JzdpPfiruPLv8BVDpbL7zl6wdhxE1du8V1wsJHlIlgHBp5mDjzfhgmeJvkf9rYWYJhL2lEeWiuFp"
+      * }
+      */
+    
+  }
+  
+  login
   
 }
--- lihaoyi/src/main/scala/request/dto/DTO.scala
+++ lihaoyi/src/main/scala/request/dto/DTO.scala
@@ -1,4 +1,5 @@
 package request.dto
+import upickle.default.{ReadWriter => RW, macroRW}
 
 case class RegRq(email: String,    // NN
                  firstName: String, // NN 
@@ -12,7 +13,7 @@ case class RegRq(email: String,    // NN
                  password: String, // NN
                 )
 object RegRq {
-  implicit val regRqRW: upickle.default.ReadWriter[RegRq] = upickle.default.macroRW
+  implicit val rw: RW[RegRq] = macroRW
 }
 
 case class RegRs(act_token:     String,
@@ -20,10 +21,8 @@ case class RegRs(act_token:     String,
                  ep_enabled:    Boolean,
                  refresh_token: String)
 
-/**
-  * "act_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3MDUiLCJleHAiOjE1OTg0Nzc5NDEsImlhdCI6MTU5ODQ3NDM0MX0.75JRrMi-v8ZM75dO8C6oSeX-FmbI7Q9BlsbufnO2_CQ",
-  * "user_Id": "0a68781a-792a-427c-8f7a-12171966a807",
-  * "ep_enabled": false,
-  * "refresh_token": "M1lSzZawwP3frINhysgEN0qnmkqpBhNK85hoioy0yqphLCCfj1dhSTn3yaWWfhIyemcLb1raD4zSmqrBQvsnwuyXLf1SGEz0lDKQS5SIFnXEiFTXMcDYC0su4zey9smb"
-  */
+case class LoginRq(email: String, password: String)
+object LoginRq {
+  implicit val rw: RW[LoginRq] = macroRW
+}
   
