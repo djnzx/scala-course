e016dd1e88a1567087eaa06aa13326e12397116d
alexr007 <alexey.rykhalskiy@gmail.com>
2020-09-10 09:12:55 +0300

    -- reader/writer more examples

---
+++ typelevel/src/main/scala/catsx/reader/ReaderConfig.scala
@@ -0,0 +1,19 @@
+package catsx.reader
+
+import cats.data.Reader
+
+object ReaderConfig extends App {
+  
+  case class Config(port: Int, threshold: Double)
+  
+  def getPort = Reader { c: Config => c.port }
+  def getThreshold: Reader[Config, Double] = Reader { _.threshold }
+  
+  def getPair: Reader[Config, (Int, Double)] =
+    for {
+      port      <- getPort
+      threshold <- getThreshold
+    } yield (port, threshold)
+    
+  val (port, threshold) = getPair(Config(12, -13.78))
+}
---
+++ typelevel/src/main/scala/catsx/state/StateEx1.scala
@@ -0,0 +1,8 @@
+package catsx.state
+
+/**
+  * https://www.slideshare.net/RaymondTay1/practical-cats
+  */
+object StateEx1 extends App {
+  
+}
