5b7743c97c015b14b11fe8fcb8f1364c28dce09b
alexr007 <alexey.rykhalskiy@gmail.com>
2020-09-24 16:49:27 +0300

    -- minor

--- scala_plain/src/main/scala/nixa/TaskR2.scala
+++ scala_plain/src/main/scala/nixa/TaskR2.scala
@@ -17,25 +17,24 @@ object TaskR2 extends App {
     }
     /** State representation */
     type MII = immutable.Map[Int, Int]
-    case class ST(map: MII, max: Int) {
-      private def withMax(mx: Int): ST = copy(max = mx)
-      private def withMap(mp: MII): ST = copy(map = mp)
+    case class State(map: MII, max: Int) {
+      private def withMax(mx: Int): State = copy(max = mx)
+      private def withMap(mp: MII): State = copy(map = mp)
 
-      def update(token: Int, time: Int): ST = 
+      def update(token: Int, time: Int): State =
         withMap(map + (token -> (time + expLim))).withMax(time)
-      def refresh(token: Int, time: Int): ST =
-        map.get(token)
-          .map(
-            Option(_)
-            .filter(time <= _)
-            .map(_ => update(token, time))
-            .getOrElse(withMap(map - token).withMax(time))
-          ).getOrElse(withMax(time))
+
+      def refresh(token: Int, time: Int): State =
+        map.get(token) match {
+          case None                => withMax(time)       // if token isn't found -> just update the time only
+          case Some(x) if time < x => update(token, time) // if found and can be updated -> update map + time 
+          case Some(_)             => withMap(map - token).withMax(time) // else -> remove token and update the time   
+        }
     }
     /** fold initial value */
-    val zero = ST(immutable.Map.empty, 0)
+    val zero = State(immutable.Map.empty, 0)
     /** fold function */
-    def process(mm: ST, cmd: Command): ST = cmd match {
+    def process(mm: State, cmd: Command): State = cmd match {
       case Create (tk, time) => mm.update(tk, time)
       case Refresh(tk, time) => mm.refresh(tk, time)
     }
@@ -43,7 +42,7 @@ object TaskR2 extends App {
     commands
       .map(Command.parse)
       .foldLeft(zero)(process) match {
-      case ST(map, last) => map.count { case (_, exp) => exp >= last }
+      case State(map, last) => map.count { case (_, exp) => exp >= last }
     }
 
   }
