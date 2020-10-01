ea2087b4712ddd4fe009aebd83ca1177c18c5a58
alexr007 <alexey.rykhalskiy@gmail.com>
2020-10-02 00:39:22 +0300

    JohnFenceV5 - fold version ....

--- scala_plain/src/main/scala/hackerrankfp/d200421_01/johnfence/JohnFenceV5fold.scala
+++ scala_plain/src/main/scala/hackerrankfp/d200421_01/johnfence/JohnFenceV5fold.scala
@@ -1,45 +1,37 @@
 package hackerrankfp.d200421_01.johnfence
 
+import pprint.{pprintln => println}
 
 object JohnFenceV5fold {
   def readLine = scala.io.StdIn.readLine()
 
-  def width(idx: Int, s: List[Int]) = s match {
-    case Nil    => idx
-    case h :: _ => idx - 1 - h
-  } 
-  
-  def calcFence(fence: Vector[Int]) = {
+  def calcFence(fc: Vector[Int]) = {
 
-    def step1(i: Int, s: List[Int], prev: Int): (Int, List[Int], Int) =
-      if (i < fence.length)
-        s match {
-          case Nil                         => step1(i+1, i::s, prev)
-          case h::_ if fence(h) < fence(i) => step1(i+1, i::s, prev)
-          case h::t  =>
-            val ma2 = prev max fence(h) * width(i, t)
-            step1(i, t, ma2)
-        }
-      else (i, s, prev)
+    def pass1(i: Int, s: List[Int], mx: Int): (List[Int], Int) = 
+      (i - fc.length, s) match {
+        case (0, _)                     => (s, mx)
+        case (_, Nil)                   => pass1(i+1, i::s, mx)
+        case (_, h::_) if fc(i) > fc(h) => pass1(i+1, i::s, mx)
+        case (_, h::Nil)                => pass1(i,   Nil,  mx max fc(h) * i)
+        case (_, h::(t@g::_))           => pass1(i,   t,    mx max fc(h) * (i -1 -g))
+      }
 
-    val (i, s, ma) = step1(0, List.empty, 0)
-
-    val (_, _, ma3) = s.foldLeft((i, s, ma)) { case ((idx, l @ h :: t, prev), _) =>
-      val ma2 = prev max fence(h) * width(idx, l)
-      (idx, t, ma2)
+    pass1(0, Nil, 0) match { case (stack, mx) =>
+      stack.foldLeft(mx) { case (pmax, h) =>
+        pmax max fc(h) * (fc.length - 1 - h)
+      }
     }
     
-    ma3
   }
 
-  def main(args: Array[String]) = {
+  def main1(args: Array[String]) = {
     val _ = readLine
     val fence = readLine.split(" ").map(_.toInt).toVector
     val max = calcFence(fence)
     println(max)
   }
 
-  def main_test1(args: Array[String]) = {
+  def main(args: Array[String]) = {
     val fence = Vector(1, 2, 3, 4, 5, 6, 5, 4, 3, 0, 4, 5, 6, 7, 8, 6, 4, 2)
     val max = calcFence(fence)
     println(max)
