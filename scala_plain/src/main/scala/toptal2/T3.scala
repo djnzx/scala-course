7384d57e126586f9ad9f05754c55e38d8e06f1a6
alexr007 <alexey.rykhalskiy@gmail.com>
2020-10-06 11:34:32 +0300

    -- Toptal3

--- scala_plain/src/main/scala/toptal2/T3.scala
+++ scala_plain/src/main/scala/toptal2/T3.scala
@@ -3,30 +3,40 @@ package toptal2
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.reflect.ClassTag
+
 object T3 {
-
-  def bToI(b: Boolean) = if (b) 1 else 0
-
-  def hasSubArrayWithSum(xs: Array[Int], k: Int): Int = {
-    xs.foldLeft((Set.empty[Int], 0, 0)) { case ((parts, sum, cnt), item) =>
-      val sum2 = sum + item
-      val delta1 = sum2 == k
-      val delta2 = parts.contains(sum2 - k)
-      (parts + sum2, sum2, cnt + bToI(delta1) + bToI(delta2))
-    }._3
-  }
-
-
-  def solution(a: Array[Int], s: Int): Int = {
-    ???
-  }
-
+  def numOfSubArraysWithMean(a: Array[Int], s: Int): Int =
+    a.lazyZip(LazyList from 1)
+      .foldLeft((0, List(0))) { case ((pi, q), (ai, i)) =>
+        val pi2 = pi + ai
+        val qi = pi2 - s * i
+        (pi2, qi :: q)
+      }._2
+      .groupMapReduce(identity)(_=>1)(_+_)
+      .withFilter { case (_, v) => v > 1 }
+      .map { case (_, v) => v * (v - 1) / 2 }
+      .sum
 }
 
 class T3Spec extends AnyFunSpec with Matchers {
   import T3._
-  it("3") {
-    1 shouldEqual 1
+  def a[A: ClassTag](aa: A*): Array[A] = aa.toArray
+  it("a") {
+    val data = Seq(
+      (a(5,3,6,2,5,5,2),4) -> 8,
+      (a(5,3,6,2,5),4) -> 4,
+      (a(5,3,6,2,5,3),4) -> 7,
+      (a(5,3,6,2),4) -> 3,
+      (a(5,3,6,2,4),4) -> 6,
+      (a(2,1,3,7,2,2,1,3),2) -> 9,
+      (a(0,8,1,7,2,6,3,5),4) -> 10,
+      (a(2,2,3,4,1,1),2) -> 4,
+      (a(2,2,2,2,2,2),2) -> 21,
+      (a(2,2,2,2),2) -> 10,
+    )
+    for {
+      ((a, s), r) <- data
+    } numOfSubArraysWithMean(a, s) shouldEqual r
   }
-  
 }
