1190f06e0ba885d24992c270345c05333b67beba
alexr007 <alexey.rykhalskiy@gmail.com>
2020-10-06 13:23:06 +0300

    -- Toptal2 N3: O(4N), O(3N), O(2N)

--- scala_plain/src/main/scala/toptal2/T3.scala
+++ scala_plain/src/main/scala/toptal2/T3.scala
@@ -5,22 +5,106 @@ import org.scalatest.matchers.should.Matchers
 
 import scala.reflect.ClassTag
 
-object T3 {
-  def numOfSubArraysWithMean(a: Array[Int], s: Int): Int =
+object T30 {
+  def zipWithInd(a: Array[Int]) = 
     a.lazyZip(LazyList from 1)
+  
+  def bi(n: Int) =
+    n * (n - 1) / 2
+}
+
+/**
+  * O(N) to get Qi
+  * O(N) to convert it Map
+  * O(N) to map to combinations
+  * O(N) to sum
+  */
+object T3A {
+  import T30._
+
+  def numOfSubArraysWithMean(a: Array[Int], s: Int) =
+    zipWithInd(a)
       .foldLeft((0, List(0))) { case ((pi, q), (ai, i)) =>
         val pi2 = pi + ai
         val qi = pi2 - s * i
-        (pi2, qi :: q)
+        (pi2, qi::q)
       }._2
-      .groupMapReduce(identity)(_=>1)(_+_)
-      .withFilter { case (_, v) => v > 1 }
-      .map { case (_, v) => v * (v - 1) / 2 }
+      .groupBy(identity)
+      .withFilter { case (_, v) => v.length > 1 }
+      .map { case (_, v) => bi(v.length) }
       .sum
 }
 
+/**
+  * O(N) to get Qi
+  * O(N) to convert it Map (and take length only)
+  * O(N) to map to combinations
+  * O(N) to sum
+  */
+object T3B {
+  import T30._
+
+  def numOfSubArraysWithMean(a: Array[Int], s: Int) =
+    zipWithInd(a)
+      .foldLeft((0, List(0))) { case ((pi, q), (ai, i)) =>
+        val pi2 = pi + ai
+        val qi = pi2 - s * i
+        (pi2, qi::q)
+      }._2
+      .groupMapReduce(identity)(_=>1)(_+_)
+      .withFilter { case (_, v) => v > 1 }
+      .map { case (_, v) => bi(v) }
+      .sum
+}
+
+/**
+  * O(N) to get Qi and put them to Map
+  * O(N) to map to combinations
+  * O(N) to sum
+  */
+object T3C {
+  import T30._
+  
+  def numOfSubArraysWithMean(a: Array[Int], s: Int): Int = {
+    zipWithInd(a)
+      .foldLeft((0, Map(0->1))) { case ((pi, q), (ai, i)) =>
+        val pi2 = pi + ai
+        val qi = pi2 - s * i
+        (pi2, q.updatedWith(qi) {
+          case None    => Some(1)
+          case Some(x) => Some(x+1)
+        })
+      }._2
+      .withFilter { case (_, v) => v > 1 }
+      .map { case (_, v) => bi(v) }
+      .sum
+  }
+}
+
+/**
+  * O(N) to get Qi and put them to Map
+  * O(N) to map to combinations and reduce to sum in one pass
+  */
+object T3D {
+  import T30._
+  
+  def numOfSubArraysWithMean(a: Array[Int], s: Int): Int =
+    zipWithInd(a)
+      .foldLeft((0, Map(0 -> 1))) { case ((pi, q), (ai, i)) =>
+        val pi2 = pi + ai
+        val qi = pi2 - s * i
+        (pi2, q.updatedWith(qi) {
+          case None => Some(1)
+          case Some(x) => Some(x + 1)
+        })
+      }._2
+      .foldLeft(0) { case (acc, (_, len)) =>
+        if (len > 1) acc+bi(len) else acc
+      }
+}
+
 class T3Spec extends AnyFunSpec with Matchers {
-  import T3._
+  import T3D._
   def a[A: ClassTag](aa: A*): Array[A] = aa.toArray
   it("a") {
     val data = Seq(
