a29a7df616838c39f193470f9b72a79db43a9195
alexr007 <alexey.rykhalskiy@gmail.com>
2020-10-27 16:48:38 +0200

    -- mainApp colored -- check needs to be integrated

--- algorithms/src/main/scala/whg/ChesApp.scala
+++ algorithms/src/main/scala/whg/ChesApp.scala
@@ -2,18 +2,6 @@ package whg
 
 object ChesApp extends App {
   
-  val c = Chess.initial
-  println(c.rep)
-  println
+  Chess.play("checkmate.txt")
   
-  c.play(Seq(
-    "e2e4",
-    "e7e5",
-    "f1c4",
-    "b8c6",
-    "d1f3",
-    "d7d6",
-    "f3f7",
-  ))
-
 }
--- algorithms/src/main/scala/whg/Chess.scala
+++ algorithms/src/main/scala/whg/Chess.scala
@@ -1,7 +1,5 @@
 package whg
 
-import fansi.Str
-
 /**
   * - possible move for each figure
   *   - general
@@ -11,7 +9,7 @@ import fansi.Str
   */
 case class Chess(private val board: Board, nextC: Color, check: Option[Color] = None) {
 
-  def nextTurn(b: Board) = copy(board = b, nextC = nextC.another)
+  def nextMove(b: Board) = copy(board = b, nextC = nextC.another)
   def chNone() = copy(check = None)
   def chBlack() = copy(check = Some(Black))
   def chWhite() = copy(check = Some(White))
@@ -29,33 +27,62 @@ case class Chess(private val board: Board, nextC: Color, check: Option[Color] = 
       .map(board.move)                // Either[String, (Board, Boolean)]
       .fold (
         msg => (this, Some(msg)),
-        { case (b, _) => (nextTurn(b), None) }
+        { case (b, _) => (nextMove(b), None) }
       )
       
   def rep = board.rep
-  
-  def play(turns: Seq[String]): Unit = turns match {
-    case Nil        => ()
-    case turn::tail => 
-      val color: Str = fansi.Color.White(nextC.toString)
-      val turns = fansi.Color.Blue(turn)
-      val msg = fansi.Color.Red("message:")
-      
-      println(s"$color is going to make turn $turns")
-      val (chess2, message) = move(turn)
-      println(chess2.board.rep)
-      message.foreach(m => println(s"$msg $m"))
-      chess2.check.foreach(c => println(s"$c: CHECK"))
-      println
-      chess2.play(tail)
-  }
-  
-  def play(fileName: String) = {
-    // read file
-    // run play(turns: Seq[String])
-  }
+
+  def play(turns: Iterator[String]): Chess = Chess.play(turns, this)
+
+  def play(turns: Iterable[String]): Chess = play(turns.iterator)
+
+  /** filename from the file located in the resources folder */
+  def play(fileName: String): Chess = play(ChessIterator.resource(fileName).map(Move.fromGiven))
 }
 
 object Chess {
+  import fansi.{Back => BG, Color => FG}
+  
   def initial = new Chess(Board.initial, White)
+  
+  def printLine() = println("-----------------------")
+  
+  def encolorColor(c: Color) = {
+    val cs = s"  ${c.toString}  "
+    c match {
+      case White => BG.Black(FG.DarkGray(cs))
+      case Black => BG.White(FG.LightGray(cs))
+    }
+  }
+
+  def beforeMove(chess: Chess, move: String) = {
+    val cs = encolorColor(chess.nextC)
+    val moves = FG.Blue(move)
+    println(s"$cs is going to make a move: $moves")
+  }
+
+  def afterMove(chess2: Chess, message: Option[String]) = {
+    val msg = FG.Red("message:")
+    println(chess2.board.rep)
+    message.foreach(m => println(s"$msg $m"))
+    chess2.check.foreach(c => println(s"$c: CHECK"))
+    printLine()
+  }
+
+  def makeMove(chess: Chess, move: String) = {
+    beforeMove(chess, move)
+    val (chess2, message) = chess.move(move)
+    afterMove(chess2, message)
+    chess2
+  }
+  
+  def play(turns: Iterator[String], ch: Chess = initial) = turns.foldLeft(ch) { makeMove }
+  
+  def play(fileName: String) = {
+    val c = Chess.initial
+    println(c.rep)
+    Chess.printLine()
+    c.play(fileName)
+  }
+  
 }
