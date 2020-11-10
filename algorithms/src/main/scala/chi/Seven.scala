d39724206aaf14efcf54d969e49705d1eb5ac6f7
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2020-11-10 17:01:29 +0200

    -- v1.3

--- algorithms/src/main/scala/chi/Seven.scala
+++ algorithms/src/main/scala/chi/Seven.scala
@@ -2,7 +2,7 @@ package chi
 
 object Seven extends App {
   
-  val r = (1 to 1000).foldLeft(1)((a, _) => (a * 7) % 10)
+  val r = (1 to 1000000).foldLeft(1)((a, _) => (a * 7) % 10)
   println(r)
   
 }
