dd268c9fb2d73192eacb433d998e3a696fe23087
alexr <alexey.rykhalskiy@gmail.com>
2020-11-20 14:10:53 +0200

    IP_validation_v2

--- scala_plain/src/main/scala/IPv4.scala
+++ scala_plain/src/main/scala/IPv4.scala
@@ -13,5 +13,15 @@ object IPv4 {
     i4 = s3.flatMap(x => Option.when(x >= 0 && x <= 255)(x))
     if i4.length == SIZE
   } yield IP(i4(0), i4(1), i4(2), i4(3))
-  
+
+  def len(a: Array[_]) = a.length == SIZE
+  def validate2(s: String): Option[IP] = Option(s)
+    .map(_.split("."))
+    .filter(len)
+    .map(_.flatMap(x => Try(x.toInt).toOption))
+    .filter(len)
+    .map(_.filter(x => x >=0 && x <= 255))
+    .filter(len)
+    .map { case Array(a, b, c, d) => IP(a, b, c, d) }
+
 }
