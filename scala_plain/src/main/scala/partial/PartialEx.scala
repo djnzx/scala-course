aafac2abf47f05f6a91ac9860c3777aeca0063a3
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2020-11-26 15:11:28 +0200

    lifting partial functions A => B | ... to A => Option[B]

---
+++ scala_plain/src/main/scala/partial/PartialEx.scala
@@ -0,0 +1,24 @@
+package partial
+
+import scala.collection.mutable.ArraySeq
+
+object PartialEx extends App {
+
+  val a = Array("first", "second")
+
+  val accessor: Int => String = a.apply
+  
+  val aa: ArraySeq.ofRef[String] = wrapRefArray(a)
+  
+  val f: Int => Option[String] = a.lift
+  
+  val x: String = a(0)
+  val y: Option[String] = a.lift(1)
+  val z: Option[String] = a.lift(2)
+  pprint.pprintln(x)
+  pprint.pprintln(y)
+  pprint.pprintln(z)
+  
+  val l = Vector("a", "b")
+  val fx: Int => Option[String] = l.lift
+}
