8064126f208d619f5b3bbb45fc8921abaa555417
Alexey <alexey.rykhalskiy@gmail.com>
2020-11-29 14:31:56 +0200

    -- doobie intro

--- degoes/src/main/scala/doobiex/DoobieApp.scala
+++ degoes/src/main/scala/doobiex/DoobieApp.scala
@@ -2,29 +2,36 @@ package doobiex
 
 import pureconfig._
 import pureconfig.generic.auto._
+import pureconfigx.{AppConfig, PgConfig}
 
 import doobie._
 import doobie.implicits._
 import cats.effect.IO
-import doobie.util.transactor.Transactor.Aux
-import pureconfigx.AppConfig
+
+import pprint.{pprintln => println}
 
 import scala.concurrent.ExecutionContext
 
 object DoobieApp extends App {
-  val c: AppConfig = ConfigSource.default.loadOrThrow[AppConfig]
+  val appConf: AppConfig = ConfigSource.default.loadOrThrow[AppConfig]
   
   implicit val cs = IO.contextShift(ExecutionContext.global)
-
-  val xa = Transactor.fromDriverManager[IO](c.db.driver, c.db.url, c.db.user, c.db.password)
+  def trans(c: PgConfig) = 
+    Transactor.fromDriverManager[IO](c.driver, c.url, c.user, c.password)
+    
+  val xa = trans(appConf.db)
 
   case class Author(id: Int, name: String)
 
   def find(id: Int): ConnectionIO[Option[Author]] =
     sql"select id, name from author where id = $id".query[Author].option
 
-  val r1 = find(123).transact(xa).unsafeRunSync
-  val r2 = find(124).transact(xa).unsafeRunSync
-  pprint.pprintln(r1)
-  pprint.pprintln(r2)
+  val app = for {
+    u1 <- find(123).transact(xa)
+    u2 <- find(124).transact(xa)
+    _  <- IO.delay { println(u1) }
+    _  <- IO.delay { println(u2) }
+  } yield ()
+  
+  app.unsafeRunSync
 }
