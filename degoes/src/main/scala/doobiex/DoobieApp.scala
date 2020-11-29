0db8093a403fa40514abfb630c7b1cda984b7750
Alexey <alexey.rykhalskiy@gmail.com>
2020-11-29 16:48:49 +0200

    -- doobie intro

--- degoes/src/main/scala/doobiex/DoobieApp.scala
+++ degoes/src/main/scala/doobiex/DoobieApp.scala
@@ -3,25 +3,27 @@ package doobiex
 import pureconfig._
 import pureconfig.generic.auto._
 import pureconfigx.{AppConfig, PgConfig}
-
 import doobie._
 import doobie.implicits._
-import cats.effect.IO
-
+import cats.effect.{ContextShift, IO}
 import pprint.{pprintln => println}
 
 import scala.concurrent.ExecutionContext
 
+/**
+  * https://tpolecat.github.io/doobie/
+  */
 object DoobieApp extends App {
-  val appConf: AppConfig = ConfigSource.default.loadOrThrow[AppConfig]
-  
-  implicit val cs = IO.contextShift(ExecutionContext.global)
-  def trans(c: PgConfig) = 
-    Transactor.fromDriverManager[IO](c.driver, c.url, c.user, c.password)
-    
-  val xa = trans(appConf.db)
-
   case class Author(id: Int, name: String)
+  
+  val appConf: AppConfig = ConfigSource.default.loadOrThrow[AppConfig]
+
+  def trans(c: PgConfig)(implicit cs: ContextShift[IO]) =
+  Transactor.fromDriverManager[IO](c.driver, c.url, c.user, c.password)
+  
+  val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  // connection on which we will run our SQL
+  val xa = trans(appConf.db)(cs)
 
   def find(id: Int): ConnectionIO[Option[Author]] =
     sql"select id, name from author where id = $id".query[Author].option
