f942eb66e10ce49795411fc6bc4dc58847b37bef
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2020-12-21 11:29:42 +0200

    string deserializer polished a bit

---
+++ typelevel/src/main/scala/shapelessx/deserial/link.md
@@ -0,0 +1,3 @@
+### materials
+
+- [good example](https://stackoverflow.com/questions/20939716/idiomatic-scala-way-of-deserializing-delimited-strings-into-case-classes/20941316#20941316)
--- typelevel/src/main/scala/shapelessx/deserial/str/StringToCase.scala
+++ typelevel/src/main/scala/shapelessx/deserial/str/StringToCase.scala
@@ -1,28 +1,31 @@
-package shapelessx
+package shapelessx.deserial.str
 
 import shapeless.Generic.Aux
-import shapeless.{::, Generic, HList, HNil}
+import shapeless.{::, HList, HNil}
 
 import scala.util.{Failure, Success, Try}
 
-/**
-  * original idea is taken here
-  * https://stackoverflow.com/questions/20939716/idiomatic-scala-way-of-deserializing-delimited-strings-into-case-classes/20941316#20941316
-  */
-object StringToCaseApp extends App {
+object StringToCase {
 
   trait Reader[+A] { self =>
     def read(s: String): Try[A]
-    def map[B](f: A => B): Reader[B] = (s: String) => self.read(s).map(f)
+
+    def map[B](f: A => B): Reader[B] = new Reader[B] {
+      override def read(s: String): Try[B] = self.read(s).map(f)
+    }
   }
 
   object Reader {
+    val EMPTY = ""
+    val DELIM = '|'
+
     /** just syntax to pick instance */
     def pick[A: Reader] = implicitly[Reader[A]]
     /** just syntax to pick instance and read to Try[A] */
     def read[A: Reader](s: String): Try[A] = pick[A].read(s)
     /** given generic representation, pick corresponding reader, read, and map to the target type */
     def pickAndRead[A, HL: Reader](generic: Aux[A, HL]) = Reader.pick[HL].map(generic.from)
+
     /** implicit instances */
     implicit val strReader = new Reader[String] {
       def read(s: String) = Success(s)
@@ -38,25 +41,25 @@ object StringToCaseApp extends App {
     }
     implicit val hNilReader = new Reader[HNil] {
       def read(s: String) = s.isEmpty match {
-        case true  => Success(HNil)
+        case true => Success(HNil)
         case false => Failure(new Exception("Expect empty"))
       }
     }
     implicit def optReader[A: Reader] = new Reader[Option[A]] {
       override def read(s: String): Try[Option[A]] = s match {
         case "" => Success(None)
-        case s  => Reader.read[A](s).map(Some[A])
+        case s => Reader.read[A](s).map(Some[A])
       }
     }
     private def take(s: String): (String, String) = {
-      val (head, rest) = s.span(_ != '|')
+      val (head, rest) = s.span(_ != DELIM)
       val tail = rest match {
-        case "" => ""
-        case t  => t.tail
+        case e@EMPTY => e
+        case t => t.tail
       }
       (head, tail)
     }
-    implicit def hListReader[H: Reader, T <: HList: Reader]: Reader[H :: T] = new Reader[H :: T] {
+    implicit def hListReader[H: Reader, T <: HList : Reader]: Reader[H :: T] = new Reader[H :: T] {
       def read(s: String) = {
         val (head: String, tail: String) = take(s)
         for {
@@ -65,16 +68,6 @@ object StringToCaseApp extends App {
         } yield a :: b
       }
     }
-  }
-
-  case class TkyLine(a: String, b: String, c: Boolean, d: Option[Double])
-  object TkyLine {
-    implicit val reader = Reader.pickAndRead(Generic[TkyLine])
-  }
-
-  Reader.read[TkyLine]("a|bb|true|3.14") match {
-    case Success(value) => pprint.pprintln(value)
-    case Failure(x) => ???
   }
 
 }
