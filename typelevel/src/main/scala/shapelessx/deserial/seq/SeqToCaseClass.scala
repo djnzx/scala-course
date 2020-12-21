693819c96b3cc3998b6914047945a08ef02c8792
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2020-12-21 13:02:30 +0200

    -- Seq[String] deserializer Either-based -- no Try, no Exceptions -- explicit error representation

--- typelevel/src/main/scala/shapelessx/deserial/seq/SeqToCaseClass.scala
+++ typelevel/src/main/scala/shapelessx/deserial/seq/SeqToCaseClass.scala
@@ -3,20 +3,28 @@ package shapelessx.deserial.seq
 import shapeless.Generic.Aux
 import shapeless.{::, HList, HNil}
 
-import scala.collection.immutable.{:: => <>}
-import scala.util.{Failure, Success, Try}
+import scala.collection.immutable.{:: => <::>}
 
 /**
   * Seq[String] based implementation (without delimiter and parsing)
-  * Try-based result. TODO: implement it with Either
+  * Either-based result
   */
 object SeqToCaseClass {
+
+  trait TransformError
+  case object TeSequenceIsEmpty extends TransformError
+  case object TeSequenceIsNotEmpty extends TransformError
+  case object TeIntTransform extends TransformError
+  case object TeDoubleTransform extends TransformError
+  case object TeBooleanTransform extends TransformError
+  
+  type Result[+A] = Either[TransformError, A]
   
   trait Reader[+A] { self =>
-    def read(ss: Seq[String]): Try[A]
+    def read(ss: Seq[String]): Result[A]
 
     def map[B](f: A => B): Reader[B] = new Reader[B] {
-      override def read(ss: Seq[String]): Try[B] = self.read(ss).map(f)
+      override def read(ss: Seq[String]): Result[B] = self.read(ss).map(f)
     }
   }
 
@@ -26,13 +34,13 @@ object SeqToCaseClass {
     /** just syntax to pick instance */
     def pick[A: Reader] = implicitly[Reader[A]]
     /** just syntax to pick instance and read to Try[A] */
-    def read[A: Reader](s: Seq[String]): Try[A] = pick[A].read(s)
+    def read[A: Reader](s: Seq[String]): Result[A] = pick[A].read(s)
     /** given generic representation, pick corresponding reader, read, and map to the target type */
     def pickAndRead[A, HL: Reader](generic: Aux[A, HL]) = Reader.pick[HL].map(generic.from)
 
-    def head[A](sa: Seq[A]): Try[A] = sa match {
-      case Nil    => Failure(new Exception("Seq is empty during entity decoding"))
-      case h <> _ => Success(h)
+    def head[A](sa: Seq[A]): Result[A] = sa match {
+      case Nil      => Left(TeSequenceIsEmpty)
+      case h <::> _ => Right(h)
     }
     
     /** implicit instances */
@@ -40,29 +48,29 @@ object SeqToCaseClass {
       def read(ss: Seq[String]) = head(ss)
     }
     implicit val intReader = new Reader[Int] {
-      def read(ss: Seq[String]) = head(ss).flatMap(x => Try(x.toInt))
+      def read(ss: Seq[String]) = head(ss).flatMap(_.toIntOption.toRight(TeIntTransform))
     }
     implicit val dblReader = new Reader[Double] {
-      def read(ss: Seq[String]) = head(ss).flatMap(x => Try(x.toDouble))
+      def read(ss: Seq[String]) = head(ss).flatMap(_.toDoubleOption.toRight(TeDoubleTransform))
     }
     implicit val bolReader = new Reader[Boolean] {
-      def read(ss: Seq[String]) = head(ss).flatMap(x => Try(x.toBoolean))
+      def read(ss: Seq[String]) = head(ss).flatMap(_.toBooleanOption.toRight(TeBooleanTransform))
     }
     implicit val hNilReader = new Reader[HNil] {
       def read(ss: Seq[String]) = ss match {
-        case Nil => Success(HNil)
-        case _   => Failure(new Exception("Seq is non-empty in the end of parsing"))
+        case Nil => Right(HNil)
+        case _   => Left(TeSequenceIsNotEmpty)
       }
     }
     implicit def optReader[A: Reader] = new Reader[Option[A]] {
-      override def read(ss: Seq[String]): Try[Option[A]] = head(ss).flatMap {
-        case EMPTY => Success(None)
+      override def read(ss: Seq[String]) = head(ss).flatMap {
+        case EMPTY => Right(None)
         case s     => Reader.read[A](Seq(s)).map(Some[A])
       } 
     }
     private def take(ss: Seq[String]): (Seq[String], Seq[String]) = ss match {
       case Nil    => (Nil, Nil)
-      case h <> t => (Seq(h), t)
+      case h <::> t => (Seq(h), t)
     } 
     implicit def hListReader[H: Reader, T <: HList : Reader]: Reader[H :: T] = new Reader[H :: T] {
       def read(ss: Seq[String]) = {
