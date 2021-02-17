65a8d205f31f14097beb563f17f9d82e266163da
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-02-17 16:14:38 +0200

    Turnstile final

--- scala_plain/src/main/scala/t20211602/Turnstile.scala
+++ scala_plain/src/main/scala/t20211602/Turnstile.scala
@@ -7,7 +7,7 @@ import scala.util.chaining.scalaUtilChainingOps
 
 object Turnstile extends App {
 
-  sealed trait Dir extends Product with Serializable
+  sealed trait Dir
   case object Enter extends Dir
   case object Exit extends Dir
   object Dir {
@@ -17,47 +17,47 @@ object Turnstile extends App {
       case _ => sys.error(s"unexpected value given $n")
     }
   }
-  case class St(time: Int, turn: Option[Dir]) {
-    def time(t: Int) = copy(time = t)
-  }
-  object St {
-    def apply(t: Int, d: Dir) = new St(t, Some(d))
-    def initial = St(Int.MinValue, None)
-  }
-  
+
   def getTimes(times: Array[Int], dirs: Array[Int]) = {
-    
+
+    case class State(time: Int, dir: Option[Dir]) {
+      def time(t: Int) = copy(time = t)
+      lazy val time1 = time + 1
+    }
+    object State {
+      def apply(t: Int, d: Dir) = new State(t, Some(d))
+    }
+
     /** (Int, Int) means (time, initial position in the array) */
-    def go(data: List[((Int, Dir), Int)], state: St, acc: List[(Int, Int)]): List[(Int, Int)] = data match {
+    def go(xs: List[((Int, Dir), Int)], state: State, acc: List[(Int, Int)]): List[(Int, Int)] = xs match {
       /** 1. list is empty - done */
       case Nil => acc
 
-      /** 2A. a :: b, s.d = a, time is close, needs to be used */
-      case ((t1, d1), idx1) :: x :: tail if state.turn.contains(d1) && state.time >= t1 - 1 =>
-        val tNext = state.time + 1
-        go(x :: tail, state.time(tNext), tNext -> idx1 :: acc)
+      /** 2. 1st item in the state */
+      case ((t, d), i) :: tail if state.dir.contains(d) && state.time >= t - 1 =>
+        val tNext = state.time1
+        go(tail, state.time(tNext), tNext -> i :: acc)
 
-      /** 2B. a :: b, s.d = b, time is close, needs to be used */
-      case x :: ((t2, d2), idx2) :: tail if state.turn.contains(d2) && state.time >= t2 - 1 =>
-        val tNext = state.time + 1
-        go(x :: tail, state.time(tNext), tNext -> idx2 :: acc)
+      /** 3. 2nd item in the state */
+      case x :: ((t, d), i) :: tail if state.dir.contains(d) && state.time >= t - 1 =>
+        val tNext = state.time1
+        go(x :: tail, state.time(tNext), tNext -> i :: acc)
 
-      /** 3. a :: b, a = b, no state */
-      case (a@((t1, d1), idx1)) :: (b@((t2, d2), idx2)) :: tail if t1 == t2 =>
-        (d1, d2) match {
-          case (d@Exit, _) => go(b :: tail, St(t1, d), t1 -> idx1 :: acc)
-          case (_, d@Exit) => go(a :: tail, St(t2, d), t2 -> idx2 :: acc)
-        }
-        
-      /** 4. 1+ item */
+      /** 4. 1st time = 2nd time, don't need to use state, but need to pick priority, one of them is Exit */
+      case (a @ ((t1, d1), idx1)) :: (b @ ((t2, d2), idx2)) :: tail if t1 == t2 => (d1, d2) match {
+        case (Exit, _) => go(b :: tail, State(t1, Exit), t1 -> idx1 :: acc)
+        case (_, Exit) => go(a :: tail, State(t2, Exit), t2 -> idx2 :: acc)
+      }
+
+      /** 5. 1+ item */
       case ((t, d), idx) :: tail =>
-        val tNext = (state.time + 1) max t
-        go(tail, St(tNext, d), tNext -> idx :: acc)
+        val tNext = state.time1 max t
+        go(tail, State(tNext, d), tNext -> idx :: acc)
 
     }
 
     (times zip dirs.map(Dir.parse) zipWithIndex).toList
-      .pipe(go(_, St.initial, Nil))
+      .pipe(go(_, State(Int.MinValue, None), Nil))
       .toArray
       .sortBy(_._2)
       .map(_._1)
