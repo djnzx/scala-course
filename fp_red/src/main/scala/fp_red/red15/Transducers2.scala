ce87e4229057de6fc55645a08fa8b080ca9f856a
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-02-19 22:49:23 +0200

    restore sentence experiments

--- fp_red/src/main/scala/fp_red/red15/Transducers2.scala
+++ fp_red/src/main/scala/fp_red/red15/Transducers2.scala
@@ -13,14 +13,34 @@ object Transducers2 extends App {
   def decomposeLine(src: String, acc: List[Sentence] = Nil): List[Sentence] = ???
 
   /**
-    * (Some(line), buf) => (Nil,                     buf + line) - don't have delimiter, just collect to the buffer
-    * (Some(line), buf) => (List(Sentence),          buf2 + ...) - one sentence extracted
-    * (Some(line), buf) => (List(sent1, sent2, ...), buf2 + ...) - more sentences extracted
-    * (None,       buf) => (List(Sentence+.),        Nil       ) - extract whatever we have from buf and build sentence          
+    * (Some(line), buf) => (None,                          buf + line) - don't have delimiter, just collect to the buffer
+    * (Some(line), buf) => (Some(List(Sentence)),          buf2 + ...) - one sentence extracted
+    * (Some(line), buf) => (Some(List(sent1, sent2, ...)), buf2 + ...) - more sentences extracted
+    * (None,       buf) => (Some(List(Sentence+.)),        Nil       ) - extract whatever we have from buf and build sentence          
     */
-  def restoreSentences(line: Option[String], buf: List[String]): (List[Sentence], List[String]) = ???
+  def restoreSentences(line: Option[String], buf: List[String]): (Option[List[Sentence]], List[String]) = ???
   
+  def processLine(line: String, buf: List[String] = Nil, acc: List[Sentence] = Nil): (List[Sentence], List[String]) =
+    line match {
+      case "" => (acc, buf)
+      case _ =>
+        line.indexOf('.') match {
+        case -1  => (acc, buf :+ line)
+        case pos =>
+          val part1 = line.substring(0, pos + 1)
+          val part2 = line.substring(pos + 1)
+          val sentence = Sentence((buf :+ part1).mkString)
+          processLine(part2, Nil, acc :+ sentence)    
+      }
+    }
+
+  pprint.pprintln(processLine("a b c"))                 // (List(), List("a b c"))
+  pprint.pprintln(processLine("a b c", List("d", "e"))) // (List(), List("d", "e", "a b c"))
+  pprint.pprintln(processLine("a.b.c."))                // (List(Sentence(value = "a."), Sentence(value = "b."), Sentence(value = "c.")), List())
+  pprint.pprintln(processLine("a.b.c"))                 // (List(Sentence(value = "a."), Sentence(value = "b.")), List("c"))
+  pprint.pprintln(processLine("a.b.c", List("was ")))   // (List(Sentence(value = "a."), Sentence(value = "b.")), List("c"))
   
+    
   
   
   
