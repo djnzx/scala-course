9e719a4b736cfabd278b8ff3765676357618d71e
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-06-10 12:09:19 +0300

    sherlock

--- algorithms/src/main/scala/hackerrank/d200515_09/SherlockAndTheValidStringApp2.scala
+++ algorithms/src/main/scala/hackerrank/d200515_09/SherlockAndTheValidStringApp2.scala
@@ -6,22 +6,19 @@ package hackerrank.d200515_09
   * O(N + D * Log D)
   * D << N
   */
-object SherlockAndTheValidStringApp extends App {
+object SherlockAndTheValidStringApp2 extends App {
   val YES = "YES"
   val NO = "NO"
 
-  def isValid(s: String): String =
-    s.toVector
-      .groupBy(identity)
-      .map { case (c, cc) => (c, cc.size) }
-      .groupBy { case (_, cnt) => cnt }
-      .map { case (cnt, v) => (cnt, v.size) }
+  def isValid(st: String) =
+    st.groupMapReduce(identity)(_ => 1)(_ + _)
+      .groupMapReduce(_._2)(_ => 1)(_ + _)
       .toVector
       .sortBy(_._1)
     match {
       case Vector(_)                              => YES // all have the same frequency
       case Vector((f0, _), (f1, 1)) if f1-f0 == 1 => YES // bigger freq - smaller freq = 1 && bigger.length = 1
-      case Vector((1, 1), _)                      => YES // smaller len = 1 && smaller freq = 1
+      case Vector((_, 1), _)                      => YES // smaller freq = 1
       case _                                      => NO
     }
 
