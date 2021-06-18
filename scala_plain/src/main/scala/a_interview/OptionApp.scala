7c33d0fa85f0a3af355ebd105184a14bca47177a
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-06-18 14:33:44 +0300

    interview

--- scala_plain/src/main/scala/a_interview/OptionApp.scala
+++ scala_plain/src/main/scala/a_interview/OptionApp.scala
@@ -3,13 +3,16 @@ package a_interview
 object OptionApp extends App {
 
   sealed trait OptionX[A] {
-    def flatMap[B](f: A => OptionX[B]): OptionX[B]
+    def flatMap[B](f: A => OptionX[B]): OptionX[B] = this match {
+      case SomeX(a) => f(a)
+      case _ => NoneX()
+    }
     def map[B](f: A => B): OptionX[B] = flatMap(a => SomeX(f(a)))
     def filter(p: A => Boolean): OptionX[A] = flatMap(a => if (p(a)) SomeX(a) else NoneX())
-    def flatten(implicit ev: A <:< OptionX[A]): OptionX[A] = flatMap(a => a)
+    def flatten[AA](implicit ev: A <:< OptionX[AA]): OptionX[AA] = flatMap(identity[A])
     def getOrElse(other: => A): A = this match {
       case SomeX(a) => a
-      case NoneX() => other
+      case _ => other
     }
   }
 
@@ -18,16 +21,8 @@ object OptionApp extends App {
     def none[A]: OptionX[A] = NoneX[A]()
   }
 
-  case class SomeX[A](a: A) extends OptionX[A] {
-    override def flatMap[B](f: A => OptionX[B]): OptionX[B] = f(a) match {
-      case SomeX(a) => SomeX(a)
-      case NoneX() => NoneX()
-    }
-  }
-
-  case class NoneX[A]() extends OptionX[A] {
-    override def flatMap[B](f: A => OptionX[B]): OptionX[B] = NoneX[B]()
-  }
+  case class SomeX[A](a: A) extends OptionX[A]
+  case class NoneX[A]() extends OptionX[A]
 
   val x = OptionX.some(5)
   val w = x.map(_ + 1)
@@ -36,5 +31,13 @@ object OptionApp extends App {
 
   println(y)
   println(z)
+
+  val op66:   OptionX[Int] = OptionX.some(66)
+  val opop66: OptionX[OptionX[Int]] = OptionX.some(op66)
+  val op66f:  OptionX[Int] = opop66.flatten
+
+  println(op66)
+  println(opop66)
+  println(op66f)
 
 }
