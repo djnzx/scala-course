c3f2005fc7b1e22bbbf5f9f3ada74a3041d06c6e
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-07-13 09:31:16 +0300

    experiments

--- algorithms/src/main/scala/_1/IteratorApp.scala
+++ algorithms/src/main/scala/_1/IteratorApp.scala
@@ -6,4 +6,11 @@ object IteratorApp extends App {
   def it = x.iterator
   it.foreach(println)
   it.foreach(println)
+  println(it0.size)
+  it0.foreach(println)
+  val (r1, r2) = (1 to 10).iterator.map(_+10).splitAt(3)
+  r1.foreach(println)
+  r2.foreach(println)
+  println("--------------")
+  println("--------------")
 }
