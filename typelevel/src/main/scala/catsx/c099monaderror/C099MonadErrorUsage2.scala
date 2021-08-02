c1f9d3c5d447614c0215253f412d3dc510f43b43
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-08-02 14:19:27 +0300

    monadError

--- typelevel/src/main/scala/catsx/c099monaderror/C099MonadErrorUsage2.scala
+++ typelevel/src/main/scala/catsx/c099monaderror/C099MonadErrorUsage2.scala
@@ -6,16 +6,18 @@ import scala.util.Try
 
 object C099MonadErrorUsage2 extends App {
 
-  def validateAdult[F[_]](age: Int)(implicit me: MonadError[F, Throwable]): F[Int] =
-    if (age >= 18) me.pure(age)
-    else          me.raiseError(sys.error(s"age must me >= 18, given: $age"))
+  def validate(age: Int) = age >= 18
+
+  def validateAndRepresent[F[_]](age: Int)(implicit me: MonadError[F, Throwable]): F[Int] =
+    if (validate(age)) me.pure(age)
+    else               me.raiseError(sys.error(s"age must be >= 18, given: $age"))
 
   type R1[A] = Either[Throwable, A]
-  val r1: R1[Int] = validateAdult[R1](18)
+  val r1: R1[Int] = validateAndRepresent[R1](18)
   println(r1)
 
   type R2[A] = Try[A]
-  val r2: R2[Int] = validateAdult[R2](19)
+  val r2: R2[Int] = validateAndRepresent[R2](19)
   println(r2)
 
 }
