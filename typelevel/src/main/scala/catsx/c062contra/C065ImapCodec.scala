2c55e91a0d106c6ae1862e117503b714c83bf1c2
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-08-02 11:13:58 +0300

    partial unification

--- typelevel/src/main/scala/catsx/c062contra/C065ImapCodec.scala
+++ typelevel/src/main/scala/catsx/c062contra/C065ImapCodec.scala
@@ -1,45 +1,83 @@
 package catsx.c062contra
 
-object C065ImapCodec extends App {
+import catsx.c062contra.C065ImapCodec.Instances
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
 
+/**
+  * general idea behind the imap, that it is
+  * just a combination of map and contramap
+  * so we automatically derive instances by appending or prepending the chain
+  */
+object C065ImapCodec {
+
+  /**
+    * it doesn't cope with failures.
+    * to deal with failures
+    * decode must have signature:
+    * def decode(raw: String): Either[Error, A]
+    */
   trait Codec[A] {
     def encode(value: A): String
     def decode(raw: String): A
-    def imap[B](f_dec: A => B, g_enc: B => A): Codec[B] = {
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = {
       val self = this
       new Codec[B] {
-        override def encode(value: B): String = self.encode(g_enc(value))
-        override def decode(raw: String): B = f_dec(self.decode(raw))
+        override def encode(value: B): String = self.encode(enc(value))
+        override def decode(raw: String): B = dec(self.decode(raw))
       }
     }
   }
 
-  implicit val stringCodec: Codec[String] = new Codec[String] {
-    def encode(value: String): String = s"<$value>"
-    def decode(raw: String): String = raw.substring(1, raw.length - 1)
+  object Instances {
+    implicit val stringCodec: Codec[String] = new Codec[String] {
+      def encode(value: String): String = s"<$value>"
+      def decode(raw: String): String = raw.substring(1, raw.length - 1)
+    }
+
+    implicit val intCodec: Codec[Int] = stringCodec.imap[Int](x => x.toInt, x => x.toString)
+    implicit val booleanCodec: Codec[Boolean] = stringCodec.imap[Boolean](x => x.toBoolean, x => x.toString)
+    implicit val doubleCodec: Codec[Double] = stringCodec.imap[Double](x => x.toDouble, x => x.toString)
+
+    /** naive implementation without any markers */
+    implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] = c.imap[Box[A]](x => Box(x), x => x.value)
   }
 
-  implicit val intCodec: Codec[Int] = stringCodec.imap[Int](x => x.toInt, x => x.toString)
-  implicit val booleanCodec: Codec[Boolean] = stringCodec.imap[Boolean](x => x.toBoolean, x => x.toString)
-  implicit val doubleCodec: Codec[Double] = stringCodec.imap[Double](x => x.toDouble, x => x.toString)
+  object Implementation {
+    def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+    def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+  }
 
-  implicit def boxCodec[A](implicit c: Codec[A]):
-  Codec[Box[A]] = c.imap[Box[A]](x => Box(x), x => x.value)
+}
 
-  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
-  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+class C065ImapCodecSpec extends AnyFunSpec with Matchers {
+  import Instances._
+  import C065ImapCodec.Implementation._
 
-  // when we encode, type can be inferred from the argument provided
-  // we DO NOT NEED to specify which type to decode
-  println(encode(1))
-  println(encode("HELLO"))
-  println(encode(Box(true)))
+  describe("derivations") {
+    it("encoding") {
+      /** during encoding, type can be inferred from the argument provided
+        * we DO NOT NEED to specify which type to decode
+        */
+      encode(1) shouldEqual "<1>"
+      encode("HELLO") shouldEqual "<HELLO>"
+      encode(Box(true)) shouldEqual "<true>"
+    }
 
-  // when we decode, there is no way to infer type (argument is always string)
-  // we NEED to specify which type to decode
-  println(decode[String]("<HELLO>"))
-  println(decode[Box[Boolean]]("<true>"))
-  // one value can be decoded into different types
-  println(decode[Box[Int]]("<1>"))
-  println(decode[Int]("<1>"))
+    /** during decoding, we MUST specify which type decode to
+      * there is no way to infer type (argument is always string)
+      */
+    it("decoding #1") {
+      decode[String]("<HELLO>") shouldEqual "HELLO"
+      decode[Box[Boolean]]("<true>") shouldEqual Box(true)
+    }
+
+    /** due to our naive Box implementation
+      * same data can be decoded to different values
+      */
+    it("decoding #2") {
+      decode[Box[Int]]("<1>") shouldEqual Box(1)
+      decode[Int]("<1>") shouldEqual 1
+    }
+  }
 }
--- typelevel/src/main/scala/catsx/c062contra/C070PartialUnification.scala
+++ typelevel/src/main/scala/catsx/c062contra/C070PartialUnification.scala
@@ -1,32 +1,43 @@
 package catsx.c062contra
 
-import cats.implicits.toFunctorOps
+import cats.implicits.{toContravariantOps, toFunctorOps}
 
+/**
+  * we are trying to create composition
+  * Int => Double => String
+  */
 object C070PartialUnification extends App {
-  /**
-    * “partial unification” is some kind of optional compiler behaviour, default, since 2.13
-    *
-    */
-  val func1: Int => Double = (x: Int) => x.toDouble
-  val func2: Double => Double = (y: Double) => y * 2
 
-  /**
-    * in order to compile this, compiler will search Functor for Function1
-    *
-    * trait Functor[F[_]] {
-    * def map[A, B](fa: F[A])(func: A => B): F[B]
-    * }
-    *
-    * trait Function1[-A, +B] {
-    * def apply(arg: A): B
-    * }
-    * type F[A] = Int => A
-    * type F[A] = A => Double
-    *
-    * this .map from cats. functor
-    */
-  val func3: Int => Double = func1 map func2
+  /** first step */
+  val f1:  Int    => Double = (x: Int)    => x.toDouble + 0.1
+  /** second step */
+  val f2:  Double => String = (y: Double) => y.toString + "!"
 
-  val r: Double = func3(3) // 6.0
-  println(r)
+  /** composition 1a, scala library */
+  val f3s1: Int    => String = f2 compose f1
+  /** composition 1b, scala library, just explicit syntax */
+  val f3s2: Int    => String = a => f2(f1(a))
+  /** composition 2, scala library */
+  val f3s3: Int    => String = f1 andThen f2
+
+  /** partial unification works fine here:
+    * Function1[Int, Double]
+    * becomes F1[Double]
+    * and we map the outcome by providing f: Double => String
+    * and get: F1[String] */
+  val f3m:  Int    => String = f1 map f2
+  println(f3m(5)) // 5.1!
+
+  /** but doesn't work here, due to left side elimination. */
+//  val func4d: Int => String = f2.contramap(f1)
+
+  /** we need to "fix" output and be able manipulating with input
+    * let's define another type */
+  type <=[B, A] = A => B
+  /** and provide compiler another type (implementation is the same!!!) */
+  val f2b: String <= Double = f2
+  /** but right now, String will be fixed, and we can operate with Double, and prepend our function */
+  val f3c: String <= Int = f2b contramap f1
+  /** and we can use our composition */
+  println(f3c(5)) // 5.1!
 }
