f314cdac21cb68e5f89e22a402805f68b3744b91
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-08-24 09:56:42 +0300

    RequestUriSyntax pulled to another module

---
+++ typelevel/src/main/scala/_http4s/uri_ideas/RequestUriSyntax.scala
@@ -0,0 +1,16 @@
+package _http4s.uri_ideas
+
+import org.http4s.Request
+import org.http4s.Uri
+
+object RequestUriSyntax {
+
+  implicit class UriAddParentSyntax(private val uri: Uri) extends AnyVal {
+    def prepend(parentUri: Uri): Uri = parentUri.resolve(uri)
+  }
+
+  implicit class RequestUriAddParentSyntax[F[_]](private val rq: Request[F]) extends AnyVal {
+    def prependUri(parentUri: Uri): Request[F] = rq.withUri(rq.uri.prepend(parentUri))
+  }
+
+}
