2cffe78e68f57566d74a83c2920d8e79d6849077
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-08-27 18:29:46 +0300

    HV extractor V1

--- plain2/src/main/scala/hv_parser/HVParser.scala
+++ plain2/src/main/scala/hv_parser/HVParser.scala
@@ -2,34 +2,65 @@ package hv_parser
 
 object HVParser {
 
-  private val interestedIn = Map(
-    "A" -> 1,
-    "B" -> 2,
-    "C" -> 3,
-  )
-
-  def find(columns: Iterable[String]): Set[(String, Int)] =
-    columns
-      .zipWithIndex
-      .filter { case (column, _) => interestedIn.contains(column) }
-      .toSet
-
   type Header = String
   type Value = String
 
-  def pick(data: Vector[Value], meta: Set[(Header, Int)]): Set[(Header, Value)] =
-    meta
+  /** It finds indexes of requested columns
+    *
+    * @param columns Iterable[String] with header names
+    * @param interested Set[String] we are interested in
+    *
+    * @return (Header, Int) column name and its position
+    */
+  def findColumns(columns: Iterable[Header], interested: Set[Header]): Set[(Header, Int)] =
+    columns
+      .zipWithIndex
+      .filter { case (column, _) => interested.contains(column) }
+      .toSet
+
+  /** pick data based on previous step (intermediate) */
+  def pickHv(data: Vector[Value], found: Set[(Header, Int)]): Set[(Header, Value)] =
+    found
       .map { case (header, index) => header -> data(index) }
 
-  def pickSorted(data: Vector[Value], meta: Set[(Header, Int)]): Seq[(Header, Value)] =
-    meta
-      .map { case (header, index) => (interestedIn(header), header -> data(index)) }
+  /** pick data based on previous step (intermediate) */
+  def pickHvSorted(
+      data: Vector[Value],
+      found: Set[(Header, Int)],
+      required: Map[Header, Int],
+    ): Vector[(Header, Value)] =
+    found
+      .map { case (header, index) => (required(header), header -> data(index)) }
       .toVector
       .sortBy { case (idx, _) => idx }
       .map { case (_, data) => data }
 
-  def pickSortedData(data: Vector[Value], meta: Set[(Header, Int)]) =
-    pickSorted(data, meta)
+  def pickVOnlySorted(data: Vector[Value], found: Set[(Header, Int)], required: Map[Header, Int]): Vector[Value] =
+    pickHvSorted(data, found, required)
       .map { case (_, v) => v }
+
+  /** First implementation based on the known size of headers length
+    * TODO: set return type to Option
+    * TODO: make it lazy in terms of headers
+    * TODO: implement checking empty string as a sign for end of the stream
+    *
+    * @param headers All the headers we have
+    * @param req All the headers we interested in
+    * @return function
+    */
+  def createMapper(headers: Seq[Header], req: Seq[Header]): IndexedSeq[Value] => Seq[Value] =
+    (data: IndexedSeq[Value]) => {
+
+      /** required columns with their indexes */
+      val required: Map[Header, Int] = req.zipWithIndex.toMap
+
+      headers
+        .zipWithIndex
+        .filter { case (column, _) => required.contains(column) }
+        .map { case (header, index) => (required(header), data(index)) }
+        .toVector
+        .sortBy { case (idx, _) => idx }
+        .map { case (_, data) => data }
+    }
 
 }
--- plain2/src/main/scala/hv_parser/HVParserSpec.scala
+++ plain2/src/main/scala/hv_parser/HVParserSpec.scala
@@ -7,6 +7,25 @@ class HVParserSpec extends AnyFunSpec with Matchers {
 
   import HVParser._
 
+  object HeadersData {
+
+    /** here is the list of columns in order we expect them to have */
+    val interestedIn0: Seq[Value] =
+      Seq("A", "B", "C")
+
+    /** represented in set to achieve O(1) contains implementation */
+    val interestedIn1: Set[Value] =
+      interestedIn0.toSet
+
+    /** represented in map to get orders */
+    val interestedIn2: Map[Header, Int] =
+      interestedIn0
+        .zipWithIndex
+        .toMap
+  }
+
+  import HeadersData._
+
   val headers = List("1", "Q", "W", "E", "A", "S", "D", "Z", "X", "C", "V", "B")
   val rawData = Vector("1", "q", "w", "e", "a", "s", "d", "z", "x", "c", "v", "b")
   val dataPositions = Set(
@@ -26,20 +45,26 @@ class HVParserSpec extends AnyFunSpec with Matchers {
   )
   val dataOnlySorted = Vector("a", "b", "c")
 
-  it("1") {
-    find(headers) shouldEqual dataPositions
+  it("find indexes of desired columns") {
+    findColumns(headers, interestedIn1) shouldEqual dataPositions
   }
 
-  it("2") {
-    pick(rawData, dataPositions) shouldEqual data
+  it("get pairs H -> V unsorted (intermediate test step)") {
+    pickHv(rawData, dataPositions) shouldEqual data
   }
 
-  it("3") {
-    pickSorted(rawData, dataPositions) shouldEqual dataSorted
+  it("get pairs H -> V sorted (intermediate test step)") {
+    pickHvSorted(rawData, dataPositions, interestedIn2) shouldEqual dataSorted
   }
 
   it("4") {
-    pickSortedData(rawData, dataPositions) shouldEqual dataOnlySorted
+    pickVOnlySorted(rawData, dataPositions, interestedIn2) shouldEqual dataOnlySorted
   }
 
+  it("remap columns - final implementation") {
+    val mapper = createMapper(headers, interestedIn0)
+
+    mapper(rawData) shouldEqual dataOnlySorted
+    mapper("qwertyuiasdfghjk".toVector.map(_.toString)) shouldEqual "tfs".toVector.map(_.toString)
+  }
 }
