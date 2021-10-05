cdd959a286395b14f16984c1f79be214366d028d
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-10-05 18:57:56 +0200

    1

--- scala_plain/src/main/scala/a_interview/ShopApp.scala
+++ scala_plain/src/main/scala/a_interview/ShopApp.scala
@@ -1,14 +1,22 @@
 package a_interview
 
 trait Tool {
-  val a = 13
+  val a = 1
 }
 
-trait Shop { _: Tool =>
-  val b = a
+trait Hammer {
+  val b = 2
 }
 
+trait Shop { _: Tool with Hammer =>
+  val c = a + b
+}
+
+/**
+  * what will be printed:
+  * 0, 1, 2, 3 ?
+  */
 object ShopApp extends App {
-  val shop = new Tool with Shop
-  println(shop.b)
+  val shop = new Tool with Shop with Hammer
+  println(shop.c)
 }
