c830dfc051962a22a7d23bb15edbe359f6f49355
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-10-16 09:42:36 +0300

    ce3

--- ce3/src/main/scala/d01/ParExperiments.scala
+++ ce3/src/main/scala/d01/ParExperiments.scala
@@ -13,6 +13,9 @@ object ParExperiments extends App {
   val hello: IO[Unit] = IO(println(s"[${ Thread.sleep(2000); Thread.currentThread.getName }] Hello"))
   val world: IO[Unit] = IO(println(s"[${ Thread.sleep(2000); Thread.currentThread.getName }] World"))
 
+  val combine = (_: Unit, _: Unit) => ()
+
+  /** make them Parallel */
   val ph: Par[Unit] = Parallel[IO].parallel(hello)
   val pw: Par[Unit] = Parallel[IO].parallel(world)
 
@@ -22,17 +25,16 @@ object ParExperiments extends App {
 //    _ <- pw
 //  } yield ()
 
-  /** still sequential because for monads, map2 is based on flatMap */
+  /** Parallel does have Applicative, so we can use mapN */
   val par_hw: Par[Unit] =
-    (ph, pw)
-      .mapN((_, _) => ())
+    (ph, pw).mapN(combine)
 
-  val hw: IO[Unit] = Parallel[IO].sequential(par_hw)
-  hw.unsafeRunSync()
+  /** make them Sequential back */
+  val r1: IO[Unit] = Parallel[IO].sequential(par_hw)
+  r1.unsafeRunSync()
 
-  // or
-  val r: IO[Unit] = (hello, world).parMapN((_, _) => ())
-
-  r.unsafeRunSync()
+  /** or do everything in one shot */
+  val r2: IO[Unit] = (hello, world).parMapN(combine)
+  r2.unsafeRunSync()
 
 }
