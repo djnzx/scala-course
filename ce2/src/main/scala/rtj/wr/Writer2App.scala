df7756da8a2bcd089dd96eb834b3aa78470916c4
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2021-12-24 14:02:49 +0200

    more writers

---
+++ ce2/src/main/scala/rtj/wr/Writer2App.scala
@@ -0,0 +1,52 @@
+package rtj.wr
+
+import cats._
+import cats.data._
+import cats.implicits._
+
+object Writer2App extends App {
+
+  def tab(n: Int) = " " * (5 - n)
+
+  def countAndSayPlain(n: Int): Unit = n match {
+    case 0 =>
+      val msg = tab(n) + "starting unwinding!"
+      println(msg)
+    case n =>
+      val msgPre = tab(n) + s"winding $n"
+      val msgPost = tab(n) + s"unwinding $n"
+
+      println(msgPre)
+      countAndSayPlain(n - 1)
+      println(msgPost)
+  }
+
+  countAndSayPlain(5)
+
+  // describe
+  def countAndSayFp(n: Int): Writer[Vector[String], Unit] = n match {
+    case 0 =>
+      // the same value
+      val msg = tab(n) + "starting unwinding!"
+      // constructing the PART of message
+      Writer(Vector(msg), ())
+    case n =>
+      // the same values
+      val msgPre = tab(n) + s"winding $n"
+      val msgPost = tab(n) + s"unwinding $n"
+      // "lifted"
+      val msgPreLifted: Writer[Vector[String], Unit] = Vector(msgPre).tell
+      val msgPostLifted: Writer[Vector[String], Unit] = Vector(msgPost).tell
+      // combined "automatically"
+      for {
+        _ <- msgPreLifted
+        _ <- countAndSayFp(n - 1)
+        _ <- msgPostLifted
+      } yield ()
+  }
+
+  //
+  val (r, _) = countAndSayFp(5).run
+  val s = r.mkString("\n")
+  println(s)
+}
