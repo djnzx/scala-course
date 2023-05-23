0cba3b4de4dd70c8917d19cbcd48d2c214adda75
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-05-23 22:01:32 +0300

    [skip ci]

--- ce3/src/main/scala/http4middle/accurate/ContextExtractor.scala
+++ ce3/src/main/scala/http4middle/accurate/ContextExtractor.scala
@@ -13,18 +13,18 @@ object ContextExtractor {
   /** general alias to express context extraction */
   type Extractor[F[_], E, A] = Request[F] => F[Either[E, A]]
 
-  private val unit: Unit = ()
+  private def toEither[A](a: Option[A]): Either[Unit, A] = a.toRight(())
 
   def either[F[_], E, A](f: Request[F] => F[Either[E, A]]): Extractor[F, E, A] = f
 
   def either[F[_]: Applicative, E, A](f: Request[F] => Either[E, A]): Extractor[F, E, A] = f.andThen(_.pure[F])
 
-  def option[F[_]: Functor, A](f: Request[F] => F[Option[A]]): Extractor[F, Unit, A] = f.andThen(_.map(_.toRight(unit)))
+  def option[F[_]: Functor, A](f: Request[F] => F[Option[A]]): Extractor[F, Unit, A] = f.andThen(_.map(toEither))
 
-  def option[F[_]: Applicative, A](f: Request[F] => Option[A]): Extractor[F, Unit, A] = either(f.andThen(_.toRight(unit)))
+  def option[F[_]: Applicative, A](f: Request[F] => Option[A]): Extractor[F, Unit, A] = either(f.andThen(toEither))
 
   def pure[F[_]: Applicative, A](f: Request[F] => A): Extractor[F, Nothing, A] = either(f.andThen(_.asRight))
 
-  def unsafe[F[_]: Applicative, A](f: Request[F] => A): Extractor[F, Throwable, A] = either { rq: Request[F] => Try(f(rq)).toEither }
+  def unsafe[F[_]: Applicative, A](f: Request[F] => A): Extractor[F, Throwable, A] = either(rq => Try(f(rq)).toEither)
 
 }
