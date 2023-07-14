74596ea8ed028ebf5fc7b4c2a02fc46592029326
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-07-15 00:49:35 +0300

    compile fix

--- degoes/src/main/scala/doobiex/DoobieApp.scala
+++ degoes/src/main/scala/doobiex/DoobieApp.scala
@@ -1,34 +1,27 @@
 package doobiex
 
-import pureconfig._
-import pureconfig.generic.auto._
-import pureconfigx.{AppConfig, PgConfig}
+import cats.effect.{ExitCode, IO, IOApp}
 import doobie._
 import doobie.implicits._
-import cats.effect.{ContextShift, IO}
 import nomicon.ch02layer.domain.Domain.{User, UserId}
 import pprint.{pprintln => println}
-
-import scala.concurrent.ExecutionContext
+import pureconfig._
+import pureconfigx.{AppConfig, PgConfig}
+import pureconfig.generic.auto._
 
 /**
   * https://tpolecat.github.io/doobie/
   */
-object DoobieApp extends App {
-  val appConf: AppConfig = ConfigSource.default.loadOrThrow[AppConfig]
+object DoobieApp extends IOApp.Simple {
+  val conf: AppConfig = ConfigSource.default.loadOrThrow[AppConfig]
 
-  def trans(c: PgConfig)(implicit cs: ContextShift[IO]) =
-    Transactor.fromDriverManager[IO](c.driver, c.url, c.user, c.password)
-  
-  val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
-  // connection on which we will run our SQL
-  val xa = trans(appConf.db)(cs)
+  def trans(c: PgConfig) = Transactor.fromDriverManager[IO](c.driver, c.url, c.user, c.password)
 
-  def find(id: Int) =
-    sql"""select id, name from "user" where id = $id""".query[User].option
+  val xa = trans(conf.db)
 
-  def create(u: User) =
-    sql"""insert into "user" (id, name) values (${u.id}, ${u.name})""".update.run
+  def find(id: Int) = sql"""select id, name from "user" where id = $id""".query[User].option
+
+  def create(u: User) = sql"""insert into "user" (id, name) values (${u.id}, ${u.name})""".update.run
   
   val app = for {
     u1 <- find(123).transact(xa)
@@ -37,6 +30,6 @@ object DoobieApp extends App {
     _  <- IO.delay { println(u2) }
     _  <- create(User(UserId(125),"Jim")).transact(xa)
   } yield ()
-  
-  app.unsafeRunSync
+
+  override def run: IO[Unit] = app
 }
--- degoes/src/main/scala/nomicon/ch02layer/services/DbConnection.scala
+++ degoes/src/main/scala/nomicon/ch02layer/services/DbConnection.scala
@@ -1,10 +1,9 @@
 package nomicon.ch02layer.services
 
-import cats.effect.{ContextShift, IO}
+import cats.effect.IO
 import doobie.Transactor
 import doobie.util.transactor.Transactor.Aux
 import nomicon.ch02layer.services.Aliases.Configuration
-import pureconfigx.AppConfig
 import zio.{UIO, URIO, ZIO}
 
 object DbConnection {
@@ -13,7 +12,7 @@ object DbConnection {
     def xa: UIO[Aux[IO, Unit]]
   }
   
-  def xa(implicit cs: ContextShift[IO]): URIO[Configuration, Aux[IO, Unit]] = ZIO.accessM { conf: Configuration =>
+  def xa: URIO[Configuration, Aux[IO, Unit]] = ZIO.accessM { conf: Configuration =>
     val confS: Configuration.Service = conf.get
     val tx: ZIO[Any, Nothing, Aux[IO, Unit]] = for {
       ac <- confS.conf
