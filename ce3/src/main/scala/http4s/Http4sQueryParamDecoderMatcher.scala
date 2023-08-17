e2e00392206b25349e10069aa5787be59500da5c
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-08-17 17:57:22 +0300

    Http4sQueryParamDecoderMatcher

--- ce3/src/main/scala/http4s/Http4sQueryParamDecoderMatcher.scala
+++ ce3/src/main/scala/http4s/Http4sQueryParamDecoderMatcher.scala
@@ -1,23 +1,19 @@
 package http4s
 
-import cats.effect.IO
-import cats.effect.IOApp
+import cats.effect.{IO, IOApp}
 import cats.implicits._
-import enumeratum.CirceEnum
-import enumeratum.Enum
-import enumeratum.EnumEntry
+import enumeratum.{CirceEnum, Enum, EnumEntry}
 import enumeratum.EnumEntry.Hyphencase
-import org.http4s.HttpRoutes
-import org.http4s.ParseFailure
-import org.http4s.QueryParamDecoder
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.io._
 import org.http4s.headers.Location
 import org.http4s.implicits._
+import org.http4s.{HttpRoutes, ParseFailure, QueryParamDecoder, QueryParameterValue}
+
 import scala.concurrent.ExecutionContext.{global => ec}
 
-object Http4sPlayGround extends IOApp.Simple {
+object Http4sQueryParamDecoderMatcher extends IOApp.Simple {
 
   sealed trait Color extends EnumEntry with Hyphencase
   object Color extends Enum[Color] with CirceEnum[Color] {
@@ -28,15 +24,28 @@ object Http4sPlayGround extends IOApp.Simple {
     case object Blue extends Color
   }
 
-  implicit val colorParamDecoder: QueryParamDecoder[Color] = pv =>
+  implicit val colorParamDecoder: QueryParamDecoder[Color] = (pv: QueryParameterValue) =>
     Color.withNameEither(pv.value).leftMap(e => ParseFailure(e.getMessage(), "")).toValidatedNel
 
   import SequenceDecoders._
-  object OptionColorParamMatcher extends OptionalQueryParamDecoderMatcher[Set[Color]]("color")
+  object OptionalColorParamMatcher extends OptionalQueryParamDecoderMatcher[List[Color]]("color")
+
+  object ListItemsMatcher extends QueryParamDecoderMatcher[List[String]]("items")
+  object ItemsMatcher extends QueryParamDecoderMatcher[String]("items")
 
   val routes = HttpRoutes.of[IO] {
-    case GET -> Root / "a" :? OptionColorParamMatcher(colors) =>
-      val folded = colors.fold("option.empty")(_.mkString("."))
+
+    case GET -> Root / "x" :? ItemsMatcher(items) =>
+      pprint.pprintln(items)
+      Ok(items)
+
+    case GET -> Root / "xs" :? ListItemsMatcher(items) =>
+      pprint.pprintln(items)
+      Ok(items)
+
+    case GET -> Root / "a" :? OptionalColorParamMatcher(colors) =>
+      pprint.pprintln(colors)
+      val folded = colors.fold("option.empty")(_.mkString(":"))
       Ok(folded)
 
     case rq @ POST -> Root / "c" =>
