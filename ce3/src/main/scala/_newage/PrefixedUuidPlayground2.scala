2ab18903ed400e4a19b8a6e4bfc171b4f1086b53
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-09-12 11:26:17 +0300

    -- ideas [skip ci]

--- ce3/src/main/scala/_newage/PrefixedUuidPlayground2.scala
+++ ce3/src/main/scala/_newage/PrefixedUuidPlayground2.scala
@@ -1,42 +1,57 @@
 package _newage
 
+import cats.Show
 import cats.implicits._
-
 import java.util.UUID
 import scala.util.Try
-import cats.Show
 
 object PrefixedUuidPlayground2 extends App {
 
   private trait IdExtractor[A] extends (A => String)
 
-  abstract class IdBase[A](private val prefix: String, private val idFn: IdExtractor[A]) {
-    implicit val show: Show[A] = (a: A) => s"$prefix${idFn(a)}"
+  abstract class IdBase[A](private val prefix: String, private val idExtractor: IdExtractor[A], private val testSuffix: String = "-test") {
+    implicit val show: Show[A] = (a: A) => s"$prefix${idExtractor(a)}"
 
-    def unapply(raw: String): Option[UUID] =
+    // uuid + test
+    def unapply(raw: String): Option[(UUID, Boolean)] =
       Option(raw)
         .collect { case s if s.startsWith(prefix) => s.substring(prefix.length) }
-        .flatMap(s => Try(UUID.fromString(s)).toOption)
+        .collect {
+          case s if s.endsWith(testSuffix) => s.substring(0, s.length - testSuffix.length) -> true
+          case s                           => s                                            -> false
+        }
+        .flatMap { case (maybeUuid, isTest) => Try(UUID.fromString(maybeUuid)).toOption.map(_ -> isTest) }
 
     def is(raw: String): Boolean = unapply(raw).fold(false)(_ => true)
   }
 
+  // not the best approach, but cleaner
+  implicit class MakeTestOps[A: Show](a: A) {
+    def test: String = implicitly[Show[A]].show(a) + "-test"
+  }
+
   case class CarId(id: UUID) extends AnyVal
-  object CarId extends IdBase[CarId]("CAR-", _.id.toString)
+  object CarId               extends IdBase[CarId]("CAR-", _.id.toString)
+
+  // TODO: how to create a test entity
 
   // usecase
-  val s = CarId(UUID.randomUUID()).show
+  val carId = CarId(UUID.randomUUID())
+  val s = carId.show
   println(s)
+  val st = carId.test
+  println(st)
 
   // matching
-  def check(raw: String): Either[String, UUID] = raw match {
+  def check(raw: String) = raw match {
     case CarId(id) => id.asRight
-    case _ => "not a car id".asLeft
+    case _         => "not a car id".asLeft
   }
 
   Seq(
     s,
+    s+"-test",
     "whatever"
   ).map(x => check(x) -> CarId.is(x))
-    .foreach(println)
+    .foreach(x => pprint.pprintln(x))
 }
