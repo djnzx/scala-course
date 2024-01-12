9225db30ba2dfc401a3c517fd396e95a4c3a265f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-01-12 13:49:39 +0200

    alphabetize - 3 different impls

--- algorithms/src/main/scala/google2/t0/Alphabetize.scala
+++ algorithms/src/main/scala/google2/t0/Alphabetize.scala
@@ -5,22 +5,44 @@ import org.scalatest.matchers.should.Matchers
 
 object Alphabetize {
 
-  def alphabetize(origin: String): String = {
+  def isToSort(c: Char) = c.isLower
+  def isToSave(c: Char) = !isToSort(c)
+
+  def alphabetizeV1(origin: String): String = {
     val L = origin.length
-    val toSort = (c: Char) => c.isLower
-    val toSave = (c: Char) => !toSort(c)
 
-    val sorted: String = origin.filter(toSort).sorted
-    val saved: Set[Int] = origin.indices.filter(i => toSave(origin(i))).toSet
+    val sorted: String      = origin.filter(isToSort).sorted
+    val toSaveInd: Set[Int] = origin.indices.filter(i => isToSave(origin(i))).toSet
 
-    def reconstruct(outcome: List[Char], constructed: Int, used: Int): String = constructed match {
-      case `L`                    => outcome.reverse.mkString
-      case i if saved.contains(i) => reconstruct(origin(i) :: outcome, i + 1, used)
-      case i                      => reconstruct(sorted(used) :: outcome, i + 1, used + 1)
+    def reconstruct(outcome: List[Char], done: Int, used: Int): String = done match {
+      case L                 => outcome.reverse.mkString
+      case i if toSaveInd(i) => reconstruct(origin(i) :: outcome, i + 1, used)
+      case i                 => reconstruct(sorted(used) :: outcome, i + 1, used + 1)
     }
 
     reconstruct(Nil, 0, 0)
   }
+
+  def alphabetizeV2(origin: String): String = {
+    val indexed = origin.zipWithIndex
+    val saved   = indexed.filter { case (c, _) => isToSave(c) }
+    val toSort  = indexed.filter { case (c, _) => isToSort(c) }
+    val sorted  = toSort.map(_._1).sorted zip toSort.map(_._2).sorted
+    (saved ++ sorted).sortBy(_._2).map(_._1).mkString
+  }
+
+  def alphabetizeV3(origin: String): String =
+    origin
+      .foldLeft(
+        List.empty[Char] -> origin.filter(isToSort).sorted.toList
+      ) {
+        case ((s2, buf), next) if isToSave(next) => (next :: s2) -> buf
+        case ((s2, next :: rest), _)             => (next :: s2) -> rest
+        case ((_, Nil), _)                       => ???
+      }
+      ._1
+      .reverse
+      .mkString
 
 }
 
@@ -36,7 +58,7 @@ class AlphabetizeSpec extends AnyFunSpec with Matchers {
 
     for {
       (in, out) <- io
-    } yield alphabetize(in) shouldEqual out
+    } yield alphabetizeV3(in) shouldEqual out
   }
 
 }
