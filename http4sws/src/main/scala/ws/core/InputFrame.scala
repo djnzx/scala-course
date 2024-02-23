5beab545561cbfb8151d94e5efd31f7a057c6a87
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-02-23 15:04:59 +0200

    -- web socket frame - inputFrame mapping done with tests

---
+++ http4sws/src/main/scala/ws/core/InputFrame.scala
@@ -0,0 +1,122 @@
+package ws.core
+
+import cats.parse.Parser0
+import org.http4s.websocket.WebSocketFrame
+
+/** the most general trait */
+sealed trait InputFrame
+
+object InputFrame {
+
+  /** 1/2. text frame */
+  sealed trait TextFrame extends InputFrame
+
+  object TextFrame {
+
+    /** 1/2. command frame */
+    sealed trait CommandFrame extends TextFrame
+
+    object CommandFrame {
+
+      /** 1/2. command invalid */
+      final case class CommandInvalid(raw: String) extends CommandFrame
+
+      /** 2/2. command valid w/parameters */
+      final case class CommandValid(cmd: String, params: List[String]) extends CommandFrame
+
+    }
+
+    /** 2/2. message frame */
+    sealed trait MessageFrame extends TextFrame
+
+    object MessageFrame {
+
+      /** 1/3. public message */
+      final case class PublicChatMessage(message: String) extends MessageFrame
+
+      /** 2/3. private message */
+      final case class PrivateChatMessage(to: String, message: String) extends MessageFrame
+
+      /** 3/3. invalid message (too short, invalid user, etc) */
+      final case class InvalidMessage(details: String) extends MessageFrame
+
+    }
+
+  }
+
+  /** 2/2. other WS frame */
+  final case class OtherFrame(wsf: WebSocketFrame) extends InputFrame
+
+  object p {
+    import TextFrame.CommandFrame._
+    import TextFrame.MessageFrame._
+    import TextFrame._
+    import cats.parse.Parser
+    import cats.parse.Parser.char
+    import cats.parse.Rfc5234.alpha
+    import cats.parse.Rfc5234.vchar
+    import cats.parse.Rfc5234.wsp
+
+    /** filter for valid commands */
+    def isCommandValid(cmd: String): Boolean = Set(
+      "help",
+      "room",
+      "rooms",
+      "members",
+    ).contains(cmd)
+
+    /** any command (detected by `/` prefix) */
+    val cmdParser: Parser[String] = char('/') *> alpha.rep.string
+
+    /** valid command (filtered by `isCommandValid`), tests only */
+    val validCmdParser: Parser[String] = cmdParser.filter(isCommandValid)
+
+    /** list of parameters */
+    val parametersParser: Parser0[List[String]] = (alpha.rep.string <* wsp.?).rep0
+
+    /** command with optional parameters */
+    val cmdWithParametersParser: Parser[(String, List[String])] = (cmdParser <* wsp.rep0) ~ parametersParser
+
+    /** command with parameters filtered by `isCommandValid` */
+    val totalCommandParser: Parser[CommandFrame] = cmdWithParametersParser
+      .map {
+        case (cmd, params) if isCommandValid(cmd) => CommandValid(cmd, params)
+        case (cmd, _)                             => CommandInvalid(cmd)
+      }
+
+    /** private message target (detected by `@` prefix) */
+    val targetParser: Parser[String] = char('@') *> alpha.rep.string
+
+    /** message with `@target` */
+    val privateMessageParser: Parser[(String, String)] = (targetParser <* wsp.rep) ~ (vchar | wsp).rep.string
+
+    /** public message */
+    val publicMessageParser: Parser[String] = (vchar | wsp).rep(2).string
+
+    /** private or public message */
+    val privateOrPublicMessageParser: Parser[MessageFrame] =
+      privateMessageParser.map { case (to, msg) => PrivateChatMessage(to, msg) } |
+        publicMessageParser.map(msg => PublicChatMessage(msg))
+
+    /** command, private or public (without InvalidMessage) */
+    val totalInputFrameParser: Parser[TextFrame] = totalCommandParser | privateOrPublicMessageParser
+
+    /** fallback - wraps everthing into InvalidMessage */
+    def fallback(raw: String): InvalidMessage = TextFrame.MessageFrame.InvalidMessage(raw)
+
+    /** total TextFrame combination */
+    def totalTextParser(text: String): TextFrame =
+      totalInputFrameParser.parseAll(text) match {
+        case Right(parsed) => parsed
+        case _             => fallback(text)
+      }
+
+  }
+
+  /** total: WebSocketFrame => InputFrame */
+  def parse(wsf: WebSocketFrame): InputFrame = wsf match {
+    case WebSocketFrame.Text(text, _) => p.totalTextParser(text)
+    case wsf                          => OtherFrame(wsf)
+  }
+
+}
---
+++ http4sws/src/main/scala/ws/core/TextFrameParserSpec.scala
@@ -0,0 +1,291 @@
+package ws.core
+
+import cats.implicits.*
+import cats.parse.Parser
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import ws.core.InputFrame.TextFrame
+
+class TextFrameParserSpec extends AnyFunSuite with Matchers with ScalaCheckPropertyChecks {
+
+  import InputFrame.p.*
+
+  test("command parser") {
+    def parse(raw: String) = cmdParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"    -> "out",
+      "/room" -> "room",
+      "/go"   -> "go",
+      "/a"    -> "a",
+    )
+
+    val dataL = Table(
+      "in",
+      "/",
+      "room",
+      "go",
+      "a",
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      val x = parse(in)
+      x.toOption shouldBe None
+    }
+  }
+
+  test("valid command parser") {
+    def parse(raw: String) = validCmdParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"       -> "out",
+      "/room"    -> "room",
+      "/rooms"   -> "rooms",
+      "/help"    -> "help",
+      "/members" -> "members",
+    )
+
+    val dataL = Table(
+      "in",
+      "/",
+      "wrong",
+      "go",
+      "a",
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      val x = parse(in)
+      x.toOption shouldBe None
+    }
+  }
+
+  test("parameters parser") {
+    def parse(raw: String) = parametersParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"       -> "out",
+      ""         -> List(),
+      "a "       -> List("a"),
+      "a bc def" -> List("a", "bc", "def"),
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+  }
+
+  test("command with parameters parser") {
+    def parse(raw: String) = cmdWithParametersParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"              -> "out",
+      "/room"           -> ("room" -> List()),
+      "/room  scala"    -> ("room" -> List("scala")),
+      "/room scala abc" -> ("room" -> List("scala", "abc")),
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+  }
+
+  test("total command parser") {
+    def parse(raw: String) = totalCommandParser.parseAll(raw)
+
+    import InputFrame.TextFrame.CommandFrame
+
+    val dataR = Table(
+      "in"              -> "out",
+      "/room"           -> CommandFrame.CommandValid("room", List()),
+      "/rooms"          -> CommandFrame.CommandValid("rooms", List()),
+      "/room scala"     -> CommandFrame.CommandValid("room", List("scala")),
+      "/room scala abc" -> CommandFrame.CommandValid("room", List("scala", "abc")),
+      "/welcome"        -> CommandFrame.CommandInvalid("welcome"),
+      "/join me"        -> CommandFrame.CommandInvalid("join"),
+    )
+    val dataL = Table(
+      "in",
+      "/",
+      "/ scala",
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      parse(in).toOption shouldBe None
+    }
+
+  }
+
+  test("target parser") {
+    def parse(raw: String): Either[Parser.Error, String] = targetParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"    -> "out",
+      "@jim"  -> "jim",
+      "@alex" -> "alex",
+      "@x"    -> "x",
+    )
+
+    val dataL = Table(
+      "in",
+      "@",
+      "room",
+      "go",
+      "a",
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      val x = parse(in)
+      x.toOption shouldBe None
+    }
+  }
+
+  test("target with message parser") {
+    def parse(raw: String) = privateMessageParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"                    -> "out",
+      "@jim hello"            -> ("jim", "hello"),
+      "@alex nice to see you" -> ("alex", "nice to see you"),
+    )
+
+    val dataL = Table(
+      "in",
+      "@ nice to see you"
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      parse(in).toOption shouldBe None
+    }
+
+  }
+
+  test("public message parser") {
+    def parse(raw: String) = publicMessageParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"                    -> "out",
+      "@jim hello"            -> "@jim hello",
+      "@alex nice to see you" -> "@alex nice to see you",
+    )
+
+    val dataL = Table(
+      "in",
+      "a"
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      parse(in).toOption shouldBe None
+    }
+
+  }
+
+  test("almost total (private or public, without Invalid Message) message parser") {
+    def parse(raw: String): Either[Parser.Error, TextFrame.MessageFrame] = privateOrPublicMessageParser.parseAll(raw)
+
+    val dataR = Table(
+      "in"                    -> "out",
+      "@jim hello"            -> TextFrame.MessageFrame.PrivateChatMessage("jim", "hello"),
+      "@alex nice to see you" -> TextFrame.MessageFrame.PrivateChatMessage("alex", "nice to see you"),
+      "hello"                 -> TextFrame.MessageFrame.PublicChatMessage("hello"),
+      "hello everybody"       -> TextFrame.MessageFrame.PublicChatMessage("hello everybody"),
+    )
+
+    val dataL = Table(
+      "in",
+      "@ hello",
+      "x"
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+    }
+
+    forAll(dataL) { in =>
+      parse(in).toOption shouldBe None
+    }
+
+  }
+
+  test("fallback wrapper") {
+    def parse(raw: String): TextFrame.MessageFrame.InvalidMessage = fallback(raw)
+
+    val dataR = Table(
+      "in",
+      "@",
+      "@",
+      "/",
+      "!@#",
+      "whatever",
+    )
+
+    forAll(dataR) { in =>
+      parse(in) shouldBe TextFrame.MessageFrame.InvalidMessage(in)
+    }
+
+  }
+
+  test("TOTAL TextParser, Invalid Message as otherwise case") {
+    import ws.core.InputFrame._
+
+    def parse(raw: String): Either[Parser.Error, TextFrame] = totalInputFrameParser.parseAll(raw)
+    def parseTotal(raw: String): TextFrame = totalTextParser(raw)
+
+    val dataR = Table(
+      "in"               -> "out",
+      "/help"            -> TextFrame.CommandFrame.CommandValid("help", List()),
+      "/room"            -> TextFrame.CommandFrame.CommandValid("room", List()),
+      "/room scala"      -> TextFrame.CommandFrame.CommandValid("room", List("scala")),
+      "/room scala abc"  -> TextFrame.CommandFrame.CommandValid("room", List("scala", "abc")),
+      "/welcome"         -> TextFrame.CommandFrame.CommandInvalid("welcome"),
+      "/join me"         -> TextFrame.CommandFrame.CommandInvalid("join"),
+      "hello world"      -> TextFrame.MessageFrame.PublicChatMessage("hello world"),
+      "@jim hi"          -> TextFrame.MessageFrame.PrivateChatMessage("jim", "hi"),
+      "@tommy follow me" -> TextFrame.MessageFrame.PrivateChatMessage("tommy", "follow me"),
+    )
+
+    val dataL = Table(
+      "in",
+      "x",
+      "",
+    )
+
+    forAll(dataR) { (in, out) =>
+      parse(in) shouldBe out.asRight
+      parseTotal(in) shouldBe out
+    }
+
+    forAll(dataL) { in =>
+      parse(in).toOption shouldBe None
+      parseTotal(in) shouldBe TextFrame.MessageFrame.InvalidMessage(in)
+    }
+
+  }
+
+}
