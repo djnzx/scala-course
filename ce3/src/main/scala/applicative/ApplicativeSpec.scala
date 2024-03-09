46a6d32318b3541f514e79ba424dd0ea54d8f98f
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-03-09 22:38:28 +0200

    -- applicative

--- ce3/src/main/scala/applicative/ApplicativeSpec.scala
+++ ce3/src/main/scala/applicative/ApplicativeSpec.scala
@@ -6,38 +6,57 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ApplicativeSpec extends AnyFunSuite {
 
-  val data1: List[Int] = List(2, 3, 4)
-  val data2: List[Int] = List(10, 20, 30, 40)
-
-  val addFn: Int => Int => Int     = x => y => x + y
-  val mulFn: Int => Int => Int     = x => y => x * y
-  val fns: List[Int => Int => Int] = List(addFn, mulFn) // len=2
-
   /** the only idea is the ability to cope with unfinished calculations */
-  test("curried") {
-    val a1: List[Int => Int] = fns.ap(data1) // len=6, 2 functions applied to 3 params.
-    pprint.pprintln(a1.length)
+  test("curried partial application") {
 
-    val a2: List[Int] = a1.ap(data2) // len=24, 6 functions applied to 4 params
-    pprint.pprintln(a2.length)
+    /** 1. having data */
+    val data123: List[Int] = List(1, 2, 3)
+    val data102030: List[Int] = List(10, 20, 30)
+
+    /** 2. having curried function */
+    val addFn: Int => Int => Int = x => y => x + y
+    val mulFn: Int => Int => Int = x => y => x * y
+
+    /** 3.1 applying partially, we have function wrapped into List */
+    val add1: List[Int => Int] = data123.map(addFn) // (+1), (+2), (+3)
+    /** 3.2 using applicative we can have many functions, and get a cartesian even here */
+    val add1a: List[Int => Int] = List(addFn, mulFn).ap(data123)
+
+    /** 4.1. now we need a monad to combine, and actually get a cartesian product !!! */
+    val add2a: List[Int] = add1a.flatMap(f => data102030.map(x => f(x)))
+    val add2b: List[Int] = data102030.flatMap(x => add1a.map(f => f(x))) // order is different
+
+    /** 4.2. but monad is not required if we have apply, even syntax is simpler */
+    val add2c: List[Int] = add1a.ap(data102030)
+
+    pprint.log(add2a)
+    pprint.log(add2b)
+    pprint.log(add2c)
   }
 
-
-  /** and special case  */
+  /** and special case */
   test("tupled") {
+    val data1: List[Int] = List(2, 3, 4)
+    val data2: List[Int] = List(10, 20, 30, 40)
+
+    val addFn: Int => Int => Int = x => y => x + y
+    val mulFn: Int => Int => Int = x => y => x * y
+    val fns: List[Int => Int => Int] = List(addFn, mulFn) // len=2
+
     val AL = Applicative[List]
 
     val partially1: List[Int] => List[Int => Int] = AL.ap(fns)
-
     val partially2: List[Int => Int] = AL.ap(fns)(data1)
-    val appliedFully: List[Int]      = AL.ap(partially2)(data2)
+    val appliedFully: List[Int] = AL.ap(partially2)(data2)
+    pprint.pprintln(appliedFully, width = 1000)
 
-    val addt                          = (x: Int, y: Int) => x + y
-    val mult                          = (x: Int, y: Int) => x * y
+    val addt = (x: Int, y: Int) => x + y
+    val mult = (x: Int, y: Int) => x * y
     val opst: List[(Int, Int) => Int] = List(addt, mult)
-    val r2: List[Int]                 = opst.ap2(data1, data2)
 
-    pprint.pprintln(appliedFully)
+    // cartesian product in one step
+    val r2: List[Int] = opst.ap2(data1, data2)
+    pprint.pprintln(r2, width = 1000)
   }
 
 }
