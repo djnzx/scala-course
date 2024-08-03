25dd1a711eabae05c83036e5e832f6163fb43fb1
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-08-03 10:27:11 +0300

    t3

---
+++ sparkx/src/main/scala/abc/ExploreApp1.scala
@@ -0,0 +1,19 @@
+package abc
+
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.connector.distributions.Distribution
+
+import scala.runtime.BoxesRunTime
+
+object ExploreApp1 extends App  {
+
+  Distribution
+  Pmod
+  Literal
+//  BoxesRunTime.boxToBoolean
+//  BoxesRunTime.boxToInteger
+  val hp = HashPartitioning2(Seq(new Expression1), 123)
+
+
+
+}
