1b8f73ee128fecf1105ab8919729ea6db6cc03e9
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-06-04 21:37:56 +0300

    multibyte playground

--- ce3/src/main/scala/utf8/Playground.scala
+++ ce3/src/main/scala/utf8/Playground.scala
@@ -12,7 +12,7 @@ object Playground extends App {
     new String(chars)
   }
 
-  def mkColored(s: String): String = {
+  def mkColoredBin(s: String): String = {
     val prefix = s.take(s.indexWhere(_ != '1') + 1)
     val payload = s.substring(prefix.length)
 
@@ -23,26 +23,39 @@ object Playground extends App {
       .toString()
   }
 
+  def mkColoredHex(s: String): String = Console.BLUE + s + Console.RESET
+
+  def groupMultibyteUtf8(bytes: Array[Byte]): Array[Array[Byte]] =
+    Iterator.unfold(0) { i =>
+      Option.when(i < bytes.length) {
+        val len = bytes(i) & 0xff match {
+          case b if (b & 0b10000000) == 0b00000000 => 1
+          case b if (b & 0b11100000) == 0b11000000 => 2
+          case b if (b & 0b11110000) == 0b11100000 => 3
+          case b if (b & 0b11111000) == 0b11110000 => 4
+          case b                                   => throw new IllegalArgumentException(s"invalid UTF-8 lead byte: ${toBin(b.toByte)}")
+        }
+        bytes.slice(i, i + len) -> (i + len)
+      }
+    }.toArray
+
   def describeUtfContent(utf: String): Unit = {
     val bytes: Array[Byte] = utf.getBytes
-    val per_char = bytes.length / utf.codePoints().count().toInt
-    val dec: Array[Int] = bytes.map(b => b & 0xff)
-    val hex: Array[String] = bytes.map(b => "%02X".format(b))
-    val bin: Array[String] = bytes.map(toBin).map(mkColored)
+    val grouped: Array[Array[Byte]] = groupMultibyteUtf8(bytes)
 
-    def groupGt1[A](xs: Iterable[A]) =
-      per_char match {
-        case 1 => xs.mkString("[", ", ", "]")
-        case _ => xs.grouped(per_char).map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]")
-      }
+    def fmt[A](xs: Array[Array[A]]): String =
+      xs.map(_.mkString("[", ", ", "]")).mkString("[", ", ", "]")
+
+    val dec = grouped.map(_.map(b => b & 0xff))
+    val hex = grouped.map(_.map(b => mkColoredHex("%02X".format(b))))
+    val bin = grouped.map(_.map(toBin).map(mkColoredBin))
 
     printf("content:       `%s`\n", utf)
     printf("length:        %d\n", utf.chars().count())
     printf("bytes length:  %d\n", bytes.length)
-    printf("bytes per char:%d\n", per_char)
-    printf("bytes decimal: %s\n", groupGt1(dec))
-    printf("bytes hex:     %s\n", groupGt1(hex))
-    printf("bytes bin:     %s\n", groupGt1(bin))
+    printf("bytes decimal: %s\n", fmt(dec))
+    printf("bytes hex:     %s\n", fmt(hex))
+    printf("bytes bin:     %s\n", fmt(bin))
     println("-" * 50)
   }
 
